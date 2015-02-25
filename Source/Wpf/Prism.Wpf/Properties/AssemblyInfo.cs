72745324dbbb0e369c991ff9fa721b5051ee0a58
Brian Lagunas <brianlagunas@users.noreply.github.com>
2015-02-25 15:06:43 -0700

    added XmlnsDefinitions to Prism.Wpf

--- Source/Wpf/Prism.Wpf/Properties/AssemblyInfo.cs
+++ Source/Wpf/Prism.Wpf/Properties/AssemblyInfo.cs
@@ -1,8 +1,10 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -21,25 +23,16 @@ using System.Windows;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-//In order to begin building localizable applications, set 
-//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-//inside a <PropertyGroup>.  For example, if you are using US english
-//in your source files, set the <UICulture> to en-US.  Then uncomment
-//the NeutralResourceLanguage attribute below.  Update the "en-US" in
-//the line below to match the UICulture setting in the project file.
+[assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguage("en")]
 
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+[assembly: XmlnsDefinition("http://www.codeplex.com/prism", "Prism.Regions")]
+[assembly: XmlnsDefinition("http://www.codeplex.com/prism", "Prism.Regions.Behaviors")]
 
+[assembly: XmlnsDefinition("http://www.codeplex.com/prism", "Prism.Mvvm")]
 
-[assembly:ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                             //(used if a resource is not found in the page, 
-                             // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                      //(used if a resource is not found in the page, 
-                                      // app, or any theme specific resource dictionaries)
-)]
-
+[assembly: XmlnsDefinition("http://www.codeplex.com/prism", "Prism.Interactivity")]
+[assembly: XmlnsDefinition("http://www.codeplex.com/prism", "Prism.Interactivity.InteractionRequest")]
 
 // Version information for an assembly consists of the following four values:
 //
