48b6648c61a3051f1a38afa057e5fe1833d3ca19
bartlannoeye <bart.lannoeye@gmail.com>
2015-03-27 00:20:04 +0100

    Addition of Autofac as container for Wpf
    Currently used linked files for sharing the mocks and
    BootstrapperFixtureBase, waiting on #19 to be merged.
    Still missing some tests.

---
+++ Source/Wpf/Prism.Autofac.Wpf.Tests/AutofacBootstrapperNullModuleCatalogFixture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prism.Modularity;
+using Prism.Wpf.Unity.Tests;
+
+namespace Prism.Autofac.Tests
+{
+    [TestClass]
+    public class AutofacBootstrapperNullModuleCatalogFixture : BootstrapperFixtureBase
+    {
+        [TestMethod]
+        public void NullModuleCatalogThrowsOnDefaultModuleInitialization()
+        {
+            var bootstrapper = new NullModuleCatalogBootstrapper();
+
+            AssertExceptionThrownOnRun(bootstrapper, typeof(InvalidOperationException), "IModuleCatalog");
+        }
+
+        private class NullModuleCatalogBootstrapper : AutofacBootstrapper
+        {
+            protected override IModuleCatalog CreateModuleCatalog()
+            {
+                return null;
+            }
+
+            protected override DependencyObject CreateShell()
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override void InitializeShell()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+    }
+}
--- Source/Wpf/Prism.Autofac.Wpf/Properties/AssemblyInfo.cs
+++ Source/Wpf/Prism.Autofac.Wpf/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -19,8 +19,14 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("70901104-ebd7-447e-b48e-0e9941f8759a")]
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page, 
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page, 
+                                              // app, or any theme specific resource dictionaries)
+)]
 
 // Version information for an assembly consists of the following four values:
 //
