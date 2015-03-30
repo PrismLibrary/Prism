09c5e329faefad1c98c75091db478cecc46dff05
Wouter de Vries <w.l.devries@gmail.com>
2015-03-30 21:54:43 +0200

    created ninject project

---
+++ Source/Wpf/Prism.Ninject.Wpf/NinjectServiceLocatorAdapter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Practices.ServiceLocation;
+using Ninject;
+
+namespace Prism.Ninject
+{
+    internal class NinjectServiceLocatorAdapter : ServiceLocatorImplBase
+    {
+        private readonly IKernel Kernel;
+
+        public NinjectServiceLocatorAdapter(IKernel kernel)
+        {
+            this.Kernel = kernel;
+        }
+
+        protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
+        {
+            return this.Kernel.GetAll(serviceType);
+        }
+
+        protected override object DoGetInstance(Type serviceType, string key)
+        {
+            return this.Kernel.Get(serviceType, name: key);
+        }
+    }
+}
