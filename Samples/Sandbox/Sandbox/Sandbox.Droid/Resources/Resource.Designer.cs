753946b0967817493f66c2bf15ca1077613920f4
Brian Lagunas <brian.lagunas@live.com>
2015-06-07 18:48:51 -0600

    upgraded to Xamarin.Forms 1.4.2.6359

--- Samples/Sandbox/Sandbox/Sandbox.Droid/Resources/Resource.Designer.cs
+++ Samples/Sandbox/Sandbox/Sandbox.Droid/Resources/Resource.Designer.cs
@@ -26,6 +26,8 @@ namespace Sandbox.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Forms.Platform.Resource.String.ApplicationName = global::Sandbox.Droid.Resource.String.ApplicationName;
+			global::Xamarin.Forms.Platform.Resource.String.Hello = global::Sandbox.Droid.Resource.String.Hello;
 		}
 		
 		public partial class Attribute
@@ -53,6 +55,25 @@ namespace Sandbox.Droid
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class String
+		{
+			
+			// aapt resource value: 0x7f030001
+			public const int ApplicationName = 2130903041;
+			
+			// aapt resource value: 0x7f030000
+			public const int Hello = 2130903040;
+			
+			static String()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private String()
 			{
 			}
 		}
