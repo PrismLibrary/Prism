fc749ce796472a4535afe1ca2562ce7c7553f82f
Brian Lagunas <brian.lagunas@live.com>
2015-06-11 18:03:57 -0700

    starting to work on UWP support

---
+++ Source/Windows10/Prism.Windows/Mvvm/ViewModelLocator.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Mvvm;
+using Windows.UI.Xaml;
+
+namespace Prism.Windows.Mvvm
+{
+    public class ViewModelLocator
+    {
+        public static readonly DependencyProperty AutowireViewModelProperty =
+            DependencyProperty.RegisterAttached("AutowireViewModel", typeof(bool), typeof(ViewModelLocator), new PropertyMetadata(false, OnAutowireViewModelChanged));
+
+        public static bool GetAutowireViewModel(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutowireViewModelProperty);
+        }
+
+        public static void SetAutowireViewModel(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutowireViewModelProperty, value);
+        }
+
+        private static void OnAutowireViewModelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if ((bool)e.NewValue)
+                ViewModelLocationProvider.AutoWireViewModelChanged(d, Bind);
+        }
+
+        private static void Bind(object view, object viewModel)
+        {
+            var element = view as FrameworkElement;
+            if (element != null)
+                element.DataContext = viewModel;
+        }
+    }
+}
