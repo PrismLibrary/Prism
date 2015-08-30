76c20e789d8b23db2196f514930734e9be297d94
veccie <veccie@gmail.com>
2015-08-30 05:59:45 +0200

    Initial solution/project setup

---
+++ Samples/Sandbox/Windows10/SplitViewNavigation/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Prism.Unity.Windows;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.UI.ViewManagement;
+
+namespace SplitViewNavigation
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : PrismUnityApplication
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(320, 320));
+
+            NavigationService.Navigate("Main", null);
+
+            return Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ Samples/Sandbox/Windows10/SplitViewNavigation/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace SplitViewNavigation.Views
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
