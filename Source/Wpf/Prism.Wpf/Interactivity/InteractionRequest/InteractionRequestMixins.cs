f68c85fcfbb8ee48bf8686ee53a9654886b78f93
Wouter de Vries <w.l.devries@gmail.com>
2015-09-04 20:06:34 +0200

    Add InteractionRequestMixins

---
+++ Source/Wpf/Prism.Wpf/Interactivity/InteractionRequest/InteractionRequestMixins.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Prism.Interactivity.InteractionRequest
+{
+    public static class InteractionRequestMixins
+    {
+        public static async Task<T> RaiseAsync<T>(this InteractionRequest<T> request, T context) where T : INotification
+        {
+            return await AwaitCallbackResult<T>(callback => request.Raise(context, callback));
+        }
+
+        private static async Task<TResult> AwaitCallbackResult<TResult>(Action<Action<TResult>> f)
+        {
+            var tcs = new TaskCompletionSource<TResult>();
+            f(n => tcs.SetResult(n));
+            return await tcs.Task;
+        }
+    }
+}
