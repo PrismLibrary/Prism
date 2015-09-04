e5fa01861dd3b5f3a3e47bf927fc51704a4c4a87
Wouter de Vries <w.l.devries@gmail.com>
2015-09-04 20:25:34 +0200

    Add intellisense comment

--- Source/Wpf/Prism.Wpf/Interactivity/InteractionRequest/InteractionRequestMixins.cs
+++ Source/Wpf/Prism.Wpf/Interactivity/InteractionRequest/InteractionRequestMixins.cs
@@ -5,6 +5,12 @@ namespace Prism.Interactivity.InteractionRequest
 {
     public static class InteractionRequestMixins
     {
+        /// <summary>
+        /// Fires the Raised event asynchronously. Please note that this request may never return
+        /// if the InteractionRequest is unhandled.
+        /// </summary>
+        /// <param name="context">The context for the interaction request.</param>
+        /// <returns>The context after the request has been handled by the UI.</returns>
         public static async Task<T> RaiseAsync<T>(this InteractionRequest<T> request, T context) where T : INotification
         {
             return await AwaitCallbackResult<T>(callback => request.Raise(context, callback));
