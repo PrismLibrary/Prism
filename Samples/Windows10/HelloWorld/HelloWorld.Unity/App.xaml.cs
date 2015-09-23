504ed341366b30a1f490393dd32cfa4d0323dd7c
bartlannoeye <bart.lannoeye@gmail.com>
2015-09-23 23:31:25 +0200

    Added dev sample for Win10 with Unity
    Proving that Unity works on with Prism for UWP.

---
+++ Samples/Windows10/HelloWorld/HelloWorld.Unity/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using HelloWorld.Services;
+using Prism.Unity.Windows;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : PrismUnityApplication
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate("Main", null);
+            return Task.FromResult<object>(null);
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            RegisterTypeIfMissing(typeof(IDataRepository), typeof(DataRepository), true);
+        }
+    }
+}
