057f07b2e5c1e9228415c452cd3a25245dc428fb
Kazuki Ohta <k_ota28@hotmail.com>
2015-10-10 23:24:32 +0900

    add PropertyChangedListener

---
+++ Source/Prism.Tests/Mvvm/PropertyChangedListenerFixture.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Mvvm;
+using Prism.Tests.Mocks.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Prism.Tests.Mvvm
+{
+    public class PropertyChangedListenerFixture
+    {
+        [Fact]
+        public void DefaultUseCase()
+        {
+            var count = 0;
+            var mockViewModel = new MockViewModel();
+            var l = new PropertyChangedListener(mockViewModel)
+            {
+                { nameof(MockViewModel.MockProperty), (s, e) => count++ }
+            };
+
+            Assert.Equal(0, count);
+            mockViewModel.MockProperty = 10;
+            Assert.Equal(1, count);
+
+            l.Dispose();
+
+            mockViewModel.MockProperty = 11;
+            Assert.Equal(1, count);
+        }
+    }
+}
---
+++ Source/Prism/Mvvm/PropertyChangedListener.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Prism.Mvvm
+{
+    /// <summary>
+    /// Subscribe PropertyChangedEvent utility. 
+    /// </summary>
+    public sealed class PropertyChangedListener : IDisposable, IEnumerable
+    {
+        private INotifyPropertyChanged Target { get; set; }
+
+        private Dictionary<string, List<PropertyChangedEventHandler>> PropertyChangeds { get; } = new Dictionary<string, List<PropertyChangedEventHandler>>();
+
+        /// <summary>
+        /// Init
+        /// </summary>
+        /// <param name="target">PropertyChanged event invoke target.</param>
+        public PropertyChangedListener(INotifyPropertyChanged target)
+        {
+            this.Target = target;
+            target.PropertyChanged += Target_PropertyChanged;
+        }
+
+        ~PropertyChangedListener()
+        {
+            this.Dispose(false);
+        }
+
+        /// <summary>
+        /// Add PropertyChangedEventHandler.
+        /// </summary>
+        /// <param name="propertyName">Target property name.</param>
+        /// <param name="h">EventHandler</param>
+        public void Add(string propertyName, PropertyChangedEventHandler h)
+        {
+            lock (PropertyChangeds)
+            {
+                var l = default(List<PropertyChangedEventHandler>);
+                if (!PropertyChangeds.TryGetValue(propertyName, out l))
+                {
+                    l = new List<PropertyChangedEventHandler>();
+                    PropertyChangeds.Add(propertyName, l);
+                }
+
+                l.Add(h);
+            }
+        }
+
+        /// <summary>
+        /// Unsubscribe all EventHandler.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        private void Target_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            lock (PropertyChangeds)
+            {
+                var l = default(List<PropertyChangedEventHandler>);
+                if (PropertyChangeds.TryGetValue(e.PropertyName, out l))
+                {
+                    foreach (var h in l)
+                    {
+                        h?.Invoke(sender, e);
+                    }
+                }
+            }
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.Target.PropertyChanged -= Target_PropertyChanged;
+                this.Target = null;
+            }
+        }
+
+        // Support collection initializer.
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return PropertyChangeds.ToArray().GetEnumerator();
+        }
+    }
+}
