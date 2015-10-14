b41338592f9646c1cf7cfe9c931a2c5669179cdc
Brian Lagunas <brian.lagunas@live.com>
2015-10-14 12:24:02 -0600

    working on Powershell script to create Nuget packages

--- Source/Wpf/Prism.Mef.Wpf/Properties/AssemblyInfo.cs
+++ Source/Wpf/Prism.Mef.Wpf/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Prism.Mef.Wpf")]
+[assembly: AssemblyProduct("Prism")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -41,15 +41,6 @@ using System.Windows;
 )]
 
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1.0.0")]
-[assembly: AssemblyFileVersion("6.1.0.0")]
+[assembly: AssemblyVersion("6.1")]
+[assembly: AssemblyFileVersion("6.1.0")]
+[assembly: AssemblyInformationalVersion("6.1")]
--- Source/Wpf/Prism.StructureMap.Wpf/Properties/AssemblyInfo.cs
+++ Source/Wpf/Prism.StructureMap.Wpf/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Windows;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Prism.StructureMap.Wpf")]
+[assembly: AssemblyProduct("Prism")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -28,15 +28,6 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1.0.0")]
-[assembly: AssemblyFileVersion("6.1.0.0")]
+[assembly: AssemblyVersion("6.1")]
+[assembly: AssemblyFileVersion("6.1.0")]
+[assembly: AssemblyInformationalVersion("6.1")]
