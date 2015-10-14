4bfe8bbf7daa42beeb9640c51e9a8e2713212568
Brian Lagunas <brian.lagunas@live.com>
2015-10-14 14:49:40 -0600

    rev'ed Prism.Windows to v 6.0
    Although Prism if in a 6.1 release, this is the first release of UWP
    support, so its version is 6.0.

--- Source/Windows10/Prism.Unity.Windows/Properties/AssemblyInfo.cs
+++ Source/Windows10/Prism.Unity.Windows/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("6.1")]
-[assembly: AssemblyFileVersion("6.1.0")]
+[assembly: AssemblyVersion("6.0")]
+[assembly: AssemblyFileVersion("6.0.0")]
 [assembly: AssemblyInformationalVersion("6.1")]
 [assembly: ComVisible(false)]
