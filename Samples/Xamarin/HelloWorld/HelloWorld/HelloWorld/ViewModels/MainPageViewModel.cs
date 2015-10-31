042b96a96ffa1c407d9bf65dc8427d80c5c4a2bc
Rob Prouse <rob@prouse.org>
2015-10-31 14:16:22 -0400

    Added a test of the Xamarin.Forms.DependencyService to the project

--- Samples/Xamarin/HelloWorld/HelloWorld/HelloWorld/ViewModels/MainPageViewModel.cs
+++ Samples/Xamarin/HelloWorld/HelloWorld/HelloWorld/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Commands;
+﻿using HelloWorld.Interfaces;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 
@@ -7,6 +8,7 @@ namespace HelloWorld.ViewModels
     public class MainPageViewModel : BindableBase
     {
         private readonly INavigationService _navigationService;
+        private readonly IMessageService _messageService;
 
         string _title = "Main Page";
         public string Title
@@ -15,11 +17,14 @@ namespace HelloWorld.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
+        public string Message { get { return _messageService.Message; } }
+
         public DelegateCommand NavigateCommand { get; set; }
 
-        public MainPageViewModel(INavigationService navigationService)
+        public MainPageViewModel(INavigationService navigationService, IMessageService messageService)
         {
             _navigationService = navigationService;
+            _messageService = messageService;
 
             NavigateCommand = new DelegateCommand(Navigate);
         }
