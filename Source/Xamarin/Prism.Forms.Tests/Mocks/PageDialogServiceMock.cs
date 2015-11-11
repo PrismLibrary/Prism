0440ac687c0b4c3a393cb34a68635f76a76d30a8
Brian Lagunas <brian.lagunas@live.com>
2015-11-10 18:23:01 -0700

    still working on tests

--- Source/Xamarin/Prism.Forms.Tests/Mocks/PageDialogServiceMock.cs
+++ Source/Xamarin/Prism.Forms.Tests/Mocks/PageDialogServiceMock.cs
@@ -7,8 +7,6 @@ namespace Prism.Forms.Tests.Mocks
     {
         private readonly string pressedButton;
 
-        public bool DisplayAlertCalled { get; set; } = false;
-
         /// <summary>
         /// Create an instance of <see cref="PageDialogServiceMock"/> with the pressed button on any alert/sheet is <paramref name="pressedButton"/>
         /// </summary>
@@ -16,17 +14,6 @@ namespace Prism.Forms.Tests.Mocks
         public PageDialogServiceMock(string pressedButton)
         {
             this.pressedButton = pressedButton;
-        }
-
-        public override Task<bool> DisplayAlert(string title, string message, string acceptButton, string cancelButton)
-        {
-            return Task.FromResult(pressedButton.Equals(acceptButton));
-        }
-
-        public override async Task DisplayAlert(string title, string message, string cancelButton)
-        {
-            if (pressedButton.Equals(cancelButton))
-                await Task.Run(() => DisplayAlertCalled = true);
         }
 
         public override Task<string> DisplayActionSheet(string title, string cancelButton, string destroyButton, params string[] otherButtons)
