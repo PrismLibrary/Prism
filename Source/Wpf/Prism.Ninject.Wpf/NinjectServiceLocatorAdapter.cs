44358f1135203df43d35cc5b709f401f388fbfd9
Brian Lagunas <brian.lagunas@live.com>
2015-11-11 18:53:31 -0700

    fixed ninject service locator file
    freaking linked files!

--- Source/Wpf/Prism.Ninject.Wpf/NinjectServiceLocatorAdapter.cs
+++ Source/Wpf/Prism.Ninject.Wpf/NinjectServiceLocatorAdapter.cs
@@ -1,8 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using Ninject;
+using Microsoft.Practices.ServiceLocation;
 
 namespace Prism.Ninject
 {
+    internal class NinjectServiceLocatorAdapter : ServiceLocatorImplBase
+    {
+        private readonly IKernel Kernel;
 
+        public NinjectServiceLocatorAdapter(IKernel kernel)
+        {
+            this.Kernel = kernel;
+        }
+
+        protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
+        {
+            return this.Kernel.GetAll(serviceType);
+        }
+
+        protected override object DoGetInstance(Type serviceType, string key)
+        {
+            return this.Kernel.Get(serviceType, name: key);
+        }
+    }
 }
