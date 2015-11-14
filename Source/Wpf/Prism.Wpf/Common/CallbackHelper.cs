3260224567732c5c381a6176591bb4bcb6d68033
Thijs Brobbel <thijsbrobbel@gmail.com>
2015-11-14 15:18:06 +0100

    Convert unneeded async methods into normal ones.
    See http://www.learnasync.net/misuse-unnecessaryasync.html for details.
    
    Fixes #303.

--- Source/Wpf/Prism.Wpf/Common/CallbackHelper.cs
+++ Source/Wpf/Prism.Wpf/Common/CallbackHelper.cs
@@ -5,11 +5,11 @@ namespace Prism.Common
 {
     internal static class CallbackHelper
     {
-        public static async Task<TResult> AwaitCallbackResult<TResult>(Action<Action<TResult>> f)
+        public static Task<TResult> AwaitCallbackResult<TResult>(Action<Action<TResult>> f)
         {
             var tcs = new TaskCompletionSource<TResult>();
             f(n => tcs.SetResult(n));
-            return await tcs.Task;
+            return tcs.Task;
         }
     }
 }
