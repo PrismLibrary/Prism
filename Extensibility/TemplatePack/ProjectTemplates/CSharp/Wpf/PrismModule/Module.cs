9fd0b854ea9ca8324b466f4faedcf290e9f1d37a
Brian Lagunas <brian.lagunas@live.com>
2016-01-14 17:47:04 -0700

    added WPF Prism Module project template

---
+++ Extensibility/TemplatePack/ProjectTemplates/CSharp/Wpf/PrismModule/Module.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Modularity;
+using Prism.Regions;
+using System;
+
+namespace $safeprojectname$
+{
+    public class $safeprojectname$Module : IModule
+    {
+        IRegionManager _regionManager;
+
+        public $safeprojectname$Module(RegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
