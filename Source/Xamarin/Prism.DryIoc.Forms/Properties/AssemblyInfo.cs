ca7c4e66d68e7548bebd8a9666afb29e53db4c97
Joakim Carselind <joakim.carselind@cub.se>
2016-04-04 08:24:44 +0200

    Updated assembly info

--- Source/Xamarin/Prism.DryIoc.Forms/Properties/AssemblyInfo.cs
+++ Source/Xamarin/Prism.DryIoc.Forms/Properties/AssemblyInfo.cs
@@ -1,30 +1,16 @@
 ﻿using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Prism.DryIoc.Forms")]
-[assembly: AssemblyDescription("Prism application with DryIoc dependency injection container")]
+[assembly: AssemblyDescription("Prism.Forms using DryIoc")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Joakim Carselind")]
 [assembly: AssemblyProduct("Prism.DryIoc.Forms")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("6.2")]
 [assembly: AssemblyFileVersion("6.2.0")]
+[assembly: AssemblyInformationalVersion("6.2.0")]
