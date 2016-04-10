f07f8c43ceb1a26d5594793e48ba46ead9d3531d
Ali Hosseini Khayat <alihk@live.com>
2016-04-10 01:11:03 -0700

    Adding project and item templates for Prism Unity UAP apps.
    Includes:
    -Project template for a blank app
    -Project template for a hamburger menu app
    -Item template for a blank page
    -Item template for a page view model

---
+++ Extensibility/TemplatePack/ProjectTemplates/CSharp/UAP/PrismUnityHamburgerMenuApp.UAP/App.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Mvvm;
+using Prism.Unity.Windows;
+using Prism.Windows.AppModel;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace $safeprojectname$
+{
+    public sealed partial class App : PrismUnityApplication
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override UIElement CreateShell(Frame rootFrame)
+        {
+            var shell = Container.Resolve<AppShell>();
+            shell.SetContentFrame(rootFrame);
+            return shell;
+        }
+
+        protected override Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+            Container.RegisterInstance<IResourceLoader>(new ResourceLoaderAdapter(new ResourceLoader()));
+
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            {
+                var viewName = viewType.FullName;
+                if (viewName.Contains(".Views."))
+                {
+                    viewName = viewName.Replace(".Views.", ".PageViewModels.");
+                }
+                else
+                {
+                    throw new ArgumentException($"The specified View type {viewName} isn't in the Views namespace.");
+                }
+
+                //// TODO: Uncomment this if ViewModels are in a separate project
+                ////var viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName.Replace("$safeprojectname$", "$safeprojectname$.ViewModel");
+                var viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
+                var suffix = viewName.EndsWith("View") ? "Model" : "ViewModel";
+                var viewModelName = $"{viewName}{suffix}, {viewAssemblyName}";
+                return Type.GetType(viewModelName);
+            });
+
+            return base.OnInitializeAsync(args);
+        }
+
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate(nameof(PageTokens.Main), null);
+            return Task.FromResult(true);
+        }
+    }
+}
