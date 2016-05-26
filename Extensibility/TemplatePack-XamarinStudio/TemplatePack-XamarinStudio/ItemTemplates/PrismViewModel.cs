6d08fb1943e08f7085272b56ba8182fe140c9585
Brian Lagunas <brian.lagunas@live.com>
2016-05-25 19:02:47 -0600

    Added item/project templates for Xamarin Studio

---
+++ Extensibility/TemplatePack-XamarinStudio/TemplatePack-XamarinStudio/ItemTemplates/PrismViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ${Namespace}.ViewModels
+{
+	public class ${Name} : BindableBase
+	{
+		public ${Name}()
+		{
+
+		}
+	}
+}
