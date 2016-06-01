a1255285123f340f5e865cb3fd40b3a2bbb7524d
Dan Siegel <me@dansiegel.net>
2016-06-01 14:55:54 -0700

    fixes a number of broken/missing xml-docs

--- Source/Wpf/Prism.StructureMap.Wpf/StructureMapExtensions.cs
+++ Source/Wpf/Prism.StructureMap.Wpf/StructureMapExtensions.cs
@@ -9,6 +9,7 @@ namespace Prism.StructureMap
         /// Registers an object for navigation.
         /// </summary>
         /// <typeparam name="T">The Type of the object to register</typeparam>
+        /// <param name="container"><see cref="IContainer"/> used to register type for Navigation</param>
         /// <param name="name">The unique name to register with the object</param>
         public static void RegisterTypeForNavigation<T>(this IContainer container, string name = null)
         {
