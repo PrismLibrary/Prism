5268301f37c3e01b06ebb5af42d310220fcd7b40
ederbond <ederbond@hotmail.com>
2016-06-12 21:39:33 -0300

    Targeting Profile 259 Refactoring AutofacExtensions methods signatures to align with Unit and Ninject conventions.

--- Source/Xamarin/Prism.Autofac.Forms/AutofacExtensions.cs
+++ Source/Xamarin/Prism.Autofac.Forms/AutofacExtensions.cs
@@ -15,20 +15,20 @@ namespace Prism.Autofac.Forms
         /// <summary>
         /// Registers a Page for navigation using a convention based approach, which uses the name of the Type being passed in as the unique name.
         /// </summary>
-        /// <typeparam name="T">The Type of Page to register</typeparam>
-        public static void RegisterTypeForNavigation<T>(this IContainer container) where T : Page
+        /// <typeparam name="TView">The Type of Page to register</typeparam>
+        public static void RegisterTypeForNavigation<TView>(this IContainer container) where TView : Page
         {
-            container.RegisterTypeForNavigation<T>(typeof(T).Name);
+            container.RegisterTypeForNavigation<TView>(typeof(TView).Name);
         }
 
         /// <summary>
         /// Registers a Page for navigation.
         /// </summary>
-        /// <typeparam name="T">The Type of Page to register</typeparam>
+        /// <typeparam name="TView">The Type of Page to register</typeparam>
         /// <param name="name">The unique name to register with the Page</param>
-        public static void RegisterTypeForNavigation<T>(this IContainer container, string name) where T : Page
+        public static void RegisterTypeForNavigation<TView>(this IContainer container, string name) where TView : Page
         {
-            Type type = typeof(T);
+            Type type = typeof(TView);
 
             container.RegisterTypeIfMissing(type, name);
 
@@ -38,15 +38,15 @@ namespace Prism.Autofac.Forms
         /// <summary>
         /// Registers a Page for navigation.
         /// </summary>
-        /// <typeparam name="T">The Type of Page to register</typeparam>
-        /// <typeparam name="C">The Class to use as the unique name for the Page</typeparam>
+        /// <typeparam name="TView">The Type of Page to register</typeparam>
+        /// <typeparam name="TViewModel">The Class to use as the unique name for the Page</typeparam>
         /// <param name="container"></param>
-        public static void RegisterTypeForNavigation<T, C>(this IContainer container)
-            where T : Page
-            where C : class
+        public static void RegisterTypeForNavigation<TView, TViewModel>(this IContainer container)
+            where TView : Page
+            where TViewModel : class
         {
-            Type type = typeof(T);
-            string name = typeof(C).FullName;
+            Type type = typeof(TView);
+            string name = typeof(TViewModel).FullName;
 
             container.RegisterTypeIfMissing(type, name);
 
