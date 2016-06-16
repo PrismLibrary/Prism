dd8633b995b3709a70df30eef3511bd6f6145207
Brian Lagunas <brian.lagunas@live.com>
2016-06-16 17:13:33 -0600

    added syncContext to CompositeCommand

---
+++ Source/Prism/Common/SynchronizationContextEventHandlerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Prism.Common
+{
+    /// <summary>
+    /// Handles the management and dispatching of EventHandlers using the SynchronizationContext
+    /// </summary>
+    public static class SynchronizationContextEventHandlerManager
+    {
+        private static readonly SynchronizationContext _synchronizationContext = SynchronizationContext.Current;
+
+        public static SynchronizationContext SynchronizationContext { get { return _synchronizationContext; } }
+
+        public static void CallHandler(object sender, EventHandler eventHandler)
+        {
+            if (eventHandler != null)
+            {
+                if (_synchronizationContext != null && _synchronizationContext != SynchronizationContext.Current)
+                    _synchronizationContext.Post((o) => eventHandler.Invoke(sender, EventArgs.Empty), null);
+                else
+                    eventHandler.Invoke(sender, EventArgs.Empty);
+            }
+        }
+    }
+}
