8229df240290bcb752c3390f6ad449533135e819
dvorn <sgd@list.ru>
2016-06-22 23:34:39 +0600

    Xml comments added.

--- Source/Wpf/Prism.Wpf/Regions/CallbackWrappers/RegionNavigationJournalWithCallbacksWrapper.cs
+++ Source/Wpf/Prism.Wpf/Regions/CallbackWrappers/RegionNavigationJournalWithCallbacksWrapper.cs
@@ -1,67 +1,111 @@
 
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Controls;
+using System.Threading.Tasks;
 
 using Prism.Regions;
-using Prism.Common;
-using System.Threading.Tasks;
 
 namespace Prism.Regions.CallbackWrappers
 {
     /// <summary>
-    /// Provides journaling of current, back, and forward navigation within regions.
+    /// Creates an instance of <see cref="IRegionNavigationJournal"/> based on an instance of <see cref="IRegionNavigationJournalWithCallbacks"/>
     /// </summary>
+    /// <seealso cref="Prism.Regions.IRegionNavigationJournal" />
     public class RegionNavigationJournalWithCallbacksWrapper : IRegionNavigationJournal
     {
         private IRegionNavigationJournalWithCallbacks _journal;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegionNavigationJournalWithCallbacksWrapper"/> class.
+        /// </summary>
+        /// <param name="journal">The underlying journal.</param>
         public RegionNavigationJournalWithCallbacksWrapper(IRegionNavigationJournalWithCallbacks journal)
         {
             _journal = journal;
         }
 
+        /// <summary>
+        /// Gets a value that indicates whether there is at least one entry in the back navigation history.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if the journal can go back; otherwise, <c>false</c>.
+        /// </value>
         public bool CanGoBack
         {
             get { return _journal.CanGoBack; }
         }
 
+        /// <summary>
+        /// Gets a value that indicates whether there is at least one entry in the forward navigation history.
+        /// </summary>
+        /// <value>
+        /// 	<c>true</c> if this instance can go forward; otherwise, <c>false</c>.
+        /// </value>
         public bool CanGoForward
         {
             get { return _journal.CanGoForward; }
         }
 
+        /// <summary>
+        /// Gets the current navigation entry of the content that is currently displayed.
+        /// </summary>
+        /// <value>The current entry.</value>
         public IRegionNavigationJournalEntry CurrentEntry
         {
             get { return _journal.CurrentEntry; }
         }
 
+        /// <summary>
+        /// Gets or sets the target that implements INavigateAsync.
+        /// </summary>
+        /// <value>The INavigate implementation.</value>
+        /// <remarks>
+        /// This is set by the owner of this journal.
+        /// </remarks>
         public INavigateAsync NavigationTarget
         {
             get { return _journal.NavigationTarget; }
             set { _journal.NavigationTarget = value; }
         }
 
+        /// <summary>
+        /// Clears the journal of current, back, and forward navigation histories.
+        /// </summary>
         public void Clear()
         {
             _journal.Clear();
         }
 
+        /// <summary>
+        /// Navigates to the most recent entry in the back navigation history, or does nothing if no entry exists in back navigation.
+        /// </summary>
+        /// <returns>The navigation result</returns>
+        /// <remarks>
+        /// If no entry exists in back navigation stack (CanGoBack false), the <see cref="NavigationContext"/> in
+        /// returned <see cref="NavigationResult"/> is set to <see langword="null"/>.
+        /// </remarks>
         public Task<NavigationResult> GoBackAsync()
         {
             _journal.GoBack();
             return Task.FromResult(new NavigationResult(null, true));
         }
 
+        /// <summary>
+        /// Navigates to the most recent entry in the forward navigation history, or does nothing if no entry exists in forward navigation.
+        /// </summary>
+        /// <returns>The navigation result</returns>
+        /// <remarks>
+        /// If no entry exists in forward navigation stack (CanGoForward false), the <see cref="NavigationContext"/> in
+        /// returned <see cref="NavigationResult"/> is set to <see langword="null"/>.
+        /// </remarks>
         public Task<NavigationResult> GoForwardAsync()
         {
             _journal.GoForward();
             return Task.FromResult(new NavigationResult(null, true));
         }
 
+        /// <summary>
+        /// Records the navigation to the entry..
+        /// </summary>
+        /// <param name="entry">The entry to record.</param>
         public void RecordNavigation(IRegionNavigationJournalEntry entry)
         {
             _journal.RecordNavigation(entry);
--- Source/Wpf/Prism.Wpf/Regions/CallbackWrappers/RegionNavigationServiceWithCallbacksWrapper.cs
+++ Source/Wpf/Prism.Wpf/Regions/CallbackWrappers/RegionNavigationServiceWithCallbacksWrapper.cs
@@ -1,58 +1,100 @@
 
 
 using System;
-using Prism.Regions;
-using Prism.Common;
 using System.Threading.Tasks;
+
+using Prism.Common;
+using Prism.Regions;
 
 namespace Prism.Regions.CallbackWrappers
 {
     /// <summary>
-    /// Provides navigation for regions.
+    /// Creates an instance of <see cref="IRegionNavigationService"/> based on an instance of <see cref="IRegionNavigationServiceWithCallbacks"/>
     /// </summary>
+    /// <seealso cref="Prism.Regions.IRegionNavigationService" />
     public class RegionNavigationServiceWithCallbacksWrapper : IRegionNavigationService
     {
         private IRegionNavigationServiceWithCallbacks _service;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegionNavigationServiceWithCallbacksWrapper"/> class.
+        /// </summary>
+        /// <param name="service">The service.</param>
         public RegionNavigationServiceWithCallbacksWrapper(IRegionNavigationServiceWithCallbacks service)
         {
             _service = service;
         }
 
+        /// <summary>
+        /// Gets the journal.
+        /// </summary>
+        /// <value>The journal.</value>
         public IRegionNavigationJournal Journal
         {
             get { return _service.Journal; }
         }
 
+        /// <summary>
+        /// Gets or sets the region owning this service.
+        /// </summary>
+        /// <value>A Region.</value>
         public IRegion Region
         {
             get { return _service.Region; }
             set { _service.Region = value; }
         }
 
+        /// <summary>
+        /// Raised when the region is navigated to content.
+        /// </summary>
         public event EventHandler<RegionNavigationEventArgs> Navigated
         {
             add { _service.Navigated += value; }
             remove { _service.Navigated -= value; }
         }
 
+        /// <summary>
+        /// Raised when the region is about to be navigated to content.
+        /// </summary>
         public event EventHandler<RegionNavigationEventArgs> Navigating
         {
             add { _service.Navigating += value; }
             remove { _service.Navigating -= value; }
         }
 
+        /// <summary>
+        /// Raised when a navigation request fails.
+        /// </summary>
         public event EventHandler<RegionNavigationFailedEventArgs> NavigationFailed
         {
             add { _service.NavigationFailed += value; }
             remove { _service.NavigationFailed -= value; }
         }
 
+        /// <summary>
+        /// Initiates navigation to the target specified by the <see cref="Uri" />.
+        /// </summary>
+        /// <param name="target">The navigation target</param>
+        /// <returns>The navigation result.</returns>
+        /// <remarks>
+        /// Convenience overloads for this method can be found as extension methods on the
+        /// <see cref="NavigationAsyncExtensions" /> class.
+        /// </remarks>
         public Task<NavigationResult> RequestNavigateAsync(Uri target)
         {
             return CallbackHelper.AwaitCallbackResult< NavigationResult>(callback => _service.RequestNavigate(target, callback));
         }
 
+        /// <summary>
+        /// Initiates navigation to the target specified by the <see cref="Uri"/>.
+        /// </summary>
+        /// <param name="target">The navigation target</param>
+        /// <param name="navigationParameters">The navigation parameters specific to the navigation request.</param>
+        /// <returns>The navigation result.</returns>
+        /// <remarks>
+        /// Convenience overloads for this method can be found as extension methods on the 
+        /// <see cref="NavigationAsyncExtensions"/> class.
+        /// </remarks>
         public Task<NavigationResult> RequestNavigateAsync(Uri target, NavigationParameters navigationParameters)
         {
             return CallbackHelper.AwaitCallbackResult<NavigationResult>(callback => _service.RequestNavigate(target, callback, navigationParameters));
