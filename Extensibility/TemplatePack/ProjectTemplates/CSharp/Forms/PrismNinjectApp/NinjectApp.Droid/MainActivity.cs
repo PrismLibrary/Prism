94eef03a18a0a43772d40278b470ce5c166d0e5d
Dan Siegel <me@dansiegel.net>
2016-06-22 22:05:32 -0700

    Updates Ninject Template for Visual Studio to include a SplashScreen on the Droid Project

--- Extensibility/TemplatePack/ProjectTemplates/CSharp/Forms/PrismNinjectApp/NinjectApp.Droid/MainActivity.cs
+++ Extensibility/TemplatePack/ProjectTemplates/CSharp/Forms/PrismNinjectApp/NinjectApp.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace $safeprojectname$
 {
-    [Activity (Label = "$saferootprojectname$", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity (Label = "$saferootprojectname$", Icon = "@drawable/icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate (Bundle bundle)
