f5ddc7b13e763db9e469912cb31de4eb6b7dcc4b
Joakim Carselind <joakim.carselind@cub.se>
2016-08-10 18:29:32 +0200

    Added tests

---
+++ Source/Xamarin/Prism.Forms.Tests/Behaviors/EventToCommandBehaviorFixture.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Prism.Behaviors;
+using Prism.Forms.Tests.Mocks.Behaviors;
+using Xamarin.Forms;
+using Xunit;
+
+namespace Prism.Forms.Tests.Behaviors
+{
+    public class EventToCommandBehaviorFixture
+    {
+        [Fact]
+        public void Command_CanExecute()
+        {
+            var behavior = new EventToCommandBehaviorMock
+            {
+                EventName = "ItemTapped",
+                Command = new Command(_ => Assert.True(false), o =>
+                {
+                    Assert.True(true);
+                    return false;
+                })
+            };
+            var listView = new ListView();
+            listView.Behaviors.Add(behavior);
+            behavior.RaiseEvent(null, null);
+        }
+
+        [Fact]
+        public void Command_Execute()
+        {
+            var behavior = new EventToCommandBehaviorMock
+            {
+                EventName = "ItemTapped",
+                Command = new Command(_ => Assert.True(true))
+            };
+            var listView = new ListView();
+            listView.Behaviors.Add(behavior);
+            behavior.RaiseEvent(null, null);
+        }
+
+        [Fact]
+        public void InvalidEventName()
+        {
+            var behavior = new EventToCommandBehavior
+            {
+                EventName = "OnItemTapped"
+            };
+            var listView = new ListView();
+            Assert.Throws<ArgumentException>(() => listView.Behaviors.Add(behavior));
+        }
+
+        [Fact]
+        public void ValidEventName()
+        {
+            var behavior = new EventToCommandBehavior
+            {
+                EventName = "ItemTapped"
+            };
+            var listView = new ListView();
+            listView.Behaviors.Add(behavior);
+        }
+    }
+}
