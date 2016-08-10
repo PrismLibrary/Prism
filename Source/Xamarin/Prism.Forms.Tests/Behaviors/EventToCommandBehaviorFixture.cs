c3d41d218e33bc5f607739fd3ec1fb5c0108c673
Joakim Carselind <joakim.carselind@cub.se>
2016-08-10 22:25:55 +0200

    Stop parameter evaluation as soon as a non-null value is found

--- Source/Xamarin/Prism.Forms.Tests/Behaviors/EventToCommandBehaviorFixture.cs
+++ Source/Xamarin/Prism.Forms.Tests/Behaviors/EventToCommandBehaviorFixture.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using Prism.Behaviors;
 using Prism.Forms.Tests.Mocks.Behaviors;
 using Xamarin.Forms;
@@ -8,6 +9,109 @@ namespace Prism.Forms.Tests.Behaviors
 {
     public class EventToCommandBehaviorFixture
     {
+        private class ItemTappedEventArgsConverter : IValueConverter
+        {
+            private readonly bool _returnParameter;
+
+            public bool HasConverted { get; private set; }
+
+            public ItemTappedEventArgsConverter(bool returnParameter)
+            {
+                _returnParameter = returnParameter;
+            }
+
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                HasConverted = true;
+                return _returnParameter ? parameter : (value as ItemTappedEventArgs)?.Item;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [Fact]
+        public void Command_OrderOfExecution()
+        {
+            const string commandParameter = "ItemProperty";
+            var converter = new ItemTappedEventArgsConverter(false);
+            var behavior = new EventToCommandBehaviorMock
+            {
+                EventName = "ItemTapped",
+                EventArgsConverter = converter,
+                CommandParameter = commandParameter,
+                Command = new Command(o =>
+                {
+                    Assert.NotNull(o);
+                    Assert.Equal(commandParameter, o);
+                    Assert.False(converter.HasConverted);
+                })
+            };
+            var listView = new ListView();
+            listView.Behaviors.Add(behavior);
+            behavior.RaiseEvent(listView, new ItemTappedEventArgs(listView, commandParameter));
+        }
+
+        [Fact]
+        public void Command_Converter()
+        {
+            const string item = "ItemProperty";
+            var behavior = new EventToCommandBehaviorMock
+            {
+                EventName = "ItemTapped",
+                EventArgsConverter = new ItemTappedEventArgsConverter(false),
+                Command = new Command(o =>
+                {
+                    Assert.NotNull(o);
+                    Assert.Equal(item, o);
+                })
+            };
+            var listView = new ListView();
+            listView.Behaviors.Add(behavior);
+            behavior.RaiseEvent(listView, new ItemTappedEventArgs(listView, item));
+        }
+
+        [Fact]
+        public void Command_ConverterWithConverterParameter()
+        {
+            const string item = "ItemProperty";
+            var behavior = new EventToCommandBehaviorMock
+            {
+                EventName = "ItemTapped",
+                EventArgsConverter = new ItemTappedEventArgsConverter(true),
+                EventArgsConverterParameter = item,
+                Command = new Command(o =>
+                {
+                    Assert.NotNull(o);
+                    Assert.Equal(item, o);
+                })
+            };
+            var listView = new ListView();
+            listView.Behaviors.Add(behavior);
+            behavior.RaiseEvent(listView, new ItemTappedEventArgs(listView, null));
+        }
+
+        [Fact]
+        public void Command_ExecuteWithParameter()
+        {
+            const string item = "ItemProperty";
+            var behavior = new EventToCommandBehaviorMock
+            {
+                EventName = "ItemTapped",
+                CommandParameter = item,
+                Command = new Command(o =>
+                {
+                    Assert.NotNull(o);
+                    Assert.Equal(item, o);
+                })
+            };
+            var listView = new ListView();
+            listView.Behaviors.Add(behavior);
+            behavior.RaiseEvent(listView, new ItemTappedEventArgs(listView, null));
+        }
+
         [Fact]
         public void Command_EventArgsParameterPath()
         {
@@ -19,7 +123,7 @@ namespace Prism.Forms.Tests.Behaviors
                 Command = new Command(o =>
                 {
                     Assert.NotNull(o);
-                    Assert.Equal("ItemProperty", o);
+                    Assert.Equal(item, o);
                 })
             };
             var listView = new ListView();
