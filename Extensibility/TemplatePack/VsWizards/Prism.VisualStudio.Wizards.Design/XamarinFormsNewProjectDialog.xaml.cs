da6263ae8ba8af900499d5a7822e2f7bc2cd131d
Brian Lagunas <brian.lagunas@live.com>
2016-08-12 15:28:53 -0600

    added new project wizard

---
+++ Extensibility/TemplatePack/VsWizards/Prism.VisualStudio.Wizards.Design/XamarinFormsNewProjectDialog.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+
+namespace Prism.VisualStudio.Wizards.Design
+{
+    /// <summary>
+    /// Interaction logic for XamarinFormsNewProjectDialog.xaml
+    /// </summary>
+    public partial class XamarinFormsNewProjectDialog : Window
+    {
+        public XamarinFormsNewProjectDialogResult Result { get; set; }
+
+
+        public XamarinFormsNewProjectDialog()
+        {
+            InitializeComponent();
+            Closed += XamarinFormsNewProjectDialog_Closed;
+        }
+
+        private void XamarinFormsNewProjectDialog_Closed(object sender, System.EventArgs e)
+        {
+            Result = new XamarinFormsNewProjectDialogResult();
+            Result.CreateAndroid = _chkAndroid.IsChecked.HasValue ? _chkAndroid.IsChecked.Value : false;
+            Result.CreateiOS = _chkiOS.IsChecked.HasValue ? _chkiOS.IsChecked.Value : false;
+            Result.CreateUwp = _chkUwp.IsChecked.HasValue ? _chkUwp.IsChecked.Value : false;
+            Result.CreateWinStore = _chkWinStore.IsChecked.HasValue ? _chkWinStore.IsChecked.Value : false;
+            Result.CreateWinPhone = _chkWinPhone.IsChecked.HasValue ? _chkWinPhone.IsChecked.Value : false;
+        }
+
+        private void _btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+
+    public class XamarinFormsNewProjectDialogResult
+    {
+        public bool CreateAndroid { get; set; }
+
+        public bool CreateiOS { get; set; }
+
+        public bool CreateUwp { get; set; }
+
+        public bool CreateWinStore { get; set; }
+
+        public bool CreateWinPhone { get; set; }
+    }
+}
---
+++ Extensibility/TemplatePack/VsWizards/Prism.VisualStudio.Wizards/XamarinFormsProjectWizard.cs
@@ -0,0 +1,68 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using Prism.VisualStudio.Wizards.Design;
+using System.Collections.Generic;
+
+namespace Prism.VisualStudio.Wizards
+{
+    public class XamarinFormsProjectWizard : IWizard
+    {
+        private EnvDTE.DTE _dte = null;
+        private string _solutionDir = null;
+        private string _templateDir = null;
+        private string _projectName = null;
+        string _container = null;
+        XamarinFormsNewProjectDialogResult _dialogResult;
+
+        public void BeforeOpeningFile(ProjectItem projectItem) { }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+            if (_dialogResult.CreateAndroid)
+                CreateProject("Droid");
+
+            if (_dialogResult.CreateiOS)
+                CreateProject("iOS");
+
+            if (_dialogResult.CreateUwp)
+                CreateProject("UWP");
+
+            if (_dialogResult.CreateWinPhone)
+                CreateProject("WinPhone");
+
+            if (_dialogResult.CreateWinStore)
+                CreateProject("Windows");
+        }
+
+        void CreateProject(string platform)
+        {
+            string name = $"{_projectName}.{platform}";
+            string projectPath = System.IO.Path.Combine(_solutionDir, System.IO.Path.Combine(_projectName, name));
+            string templateName = $"{_container}App.{platform}";
+            string templatePath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(_templateDir), $"{templateName}.zip\\{templateName}.vstemplate");
+            _dte.Solution.AddFromTemplate(templatePath, projectPath, name);
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem) { }
+
+        public void RunFinished() { }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            _dte = automationObject as EnvDTE.DTE;
+            _projectName = replacementsDictionary["$safeprojectname$"];
+            _container = replacementsDictionary["$container$"];
+            _solutionDir = System.IO.Path.GetDirectoryName(replacementsDictionary["$destinationdirectory$"]);
+            _templateDir = System.IO.Path.GetDirectoryName(customParams[0] as string);
+
+            XamarinFormsNewProjectDialog dialog = new XamarinFormsNewProjectDialog();
+            dialog.ShowDialog();
+            _dialogResult = dialog.Result;
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
