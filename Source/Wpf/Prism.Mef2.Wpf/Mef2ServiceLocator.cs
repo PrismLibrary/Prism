accd9ca41705625509fd6c4afc3ea30a8ffac33b
Sergey Seletsky <SeletskySergey@live.com>
2016-08-23 16:57:42 +0300

    Added MEF2 Bootstrapper

---
+++ Source/Wpf/Prism.Mef2.Wpf/Mef2ServiceLocator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition.Hosting;
+using Microsoft.Practices.ServiceLocation;
+
+namespace Prism.Mef2.Wpf
+{
+    public class Mef2ServiceLocator : ServiceLocatorImplBase
+    {
+        public static CompositionHost Host;
+
+        protected override object DoGetInstance(Type serviceType, string key)
+        {
+            return Host.GetExport(serviceType, key);
+        }
+
+        protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
+        {
+            return Host.GetExports(serviceType);
+        }
+    }
+}
