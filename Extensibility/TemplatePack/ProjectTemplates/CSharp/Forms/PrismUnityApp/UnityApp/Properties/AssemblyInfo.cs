4500737569e5b21d3c3e92e1aa14dc8dfe852692
Brian Lagunas <brian.lagunas@live.com>
2016-10-18 22:42:23 -0600

    fixed missing ;

--- Extensibility/TemplatePack/ProjectTemplates/CSharp/Forms/PrismUnityApp/UnityApp/Properties/AssemblyInfo.cs
+++ Extensibility/TemplatePack/ProjectTemplates/CSharp/Forms/PrismUnityApp/UnityApp/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Xamarin.Forms.Xaml
+using Xamarin.Forms.Xaml;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
