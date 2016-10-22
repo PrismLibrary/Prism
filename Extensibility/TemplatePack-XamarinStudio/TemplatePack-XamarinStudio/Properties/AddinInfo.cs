b5da79f1cba5f63bf64888d1a46c22e965c0a522
Brian Lagunas <brianlagunas@Brians-Mac-mini.local>
2016-10-22 13:11:29 -0600

    updated package versions

--- Extensibility/TemplatePack-XamarinStudio/TemplatePack-XamarinStudio/Properties/AddinInfo.cs
+++ Extensibility/TemplatePack-XamarinStudio/TemplatePack-XamarinStudio/Properties/AddinInfo.cs
@@ -2,7 +2,7 @@
 using Mono.Addins;
 using Mono.Addins.Description;
 
-[assembly:Addin ("PrismTemplatePack", Namespace = "Prism.Extensibility", Version = "1.4")]
+[assembly:Addin ("PrismTemplatePack", Namespace = "Prism.Extensibility", Version = "1.5")]
 
 [assembly:AddinName ("Prism Template Pack")]
 [assembly:AddinCategory ("IDE extensions")]
