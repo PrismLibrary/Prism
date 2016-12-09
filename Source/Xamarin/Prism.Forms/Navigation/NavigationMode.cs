60ec3b7271cdcc158156683bbac47f2996983cc1
jerrynixon <jerry@nixoncorp.com>
2016-12-09 09:15:52 -0700

    Extended to support all UWP scenarios
    Forward is a valid mode in UWP, as is Refresh (and important to Template
    10, too).

--- Source/Xamarin/Prism.Forms/Navigation/NavigationMode.cs
+++ Source/Xamarin/Prism.Forms/Navigation/NavigationMode.cs
@@ -2,7 +2,22 @@
 {
     public enum NavigationMode
     {        
-        Back,
-        New
+        //
+        // Summary:
+        //     Navigation is to a new instance of a page (not going forward or backward in the
+        //     stack).
+        New = 0,
+        //
+        // Summary:
+        //     Navigation is going backward in the stack.
+        Back = 1,
+        //
+        // Summary:
+        //     Navigation is going forward in the stack.
+        Forward = 2,
+        //
+        // Summary:
+        //     Navigation is to the current page (perhaps with different data).
+        Refresh = 3
     }
 }
