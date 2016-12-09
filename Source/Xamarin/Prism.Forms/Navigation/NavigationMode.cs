977497af90578ef1b2fe16ece2713385db2f690d
jerrynixon <jerry@nixoncorp.com>
2016-12-09 16:18:27 -0700

    Undoing Mode in this branch

--- Source/Xamarin/Prism.Forms/Navigation/NavigationMode.cs
+++ Source/Xamarin/Prism.Forms/Navigation/NavigationMode.cs
@@ -2,22 +2,7 @@
 {
     public enum NavigationMode
     {        
-        //
-        // Summary:
-        //     Navigation is to a new instance of a page (not going forward or backward in the
-        //     stack).
-        New = 0,
-        //
-        // Summary:
-        //     Navigation is going backward in the stack.
-        Back = 1,
-        //
-        // Summary:
-        //     Navigation is going forward in the stack.
-        Forward = 2,
-        //
-        // Summary:
-        //     Navigation is to the current page (perhaps with different data).
-        Refresh = 3
+        Back,
+        New
     }
 }
