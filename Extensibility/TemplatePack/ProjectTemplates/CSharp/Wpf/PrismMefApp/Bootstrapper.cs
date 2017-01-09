ad48eb847af95a744315b9fe26d02b3cda1fec62
nuits.jp <nuits.jp@live.jp>
2017-01-09 18:55:08 +0900

    Add project template for Prism.Mef.Wpf

---
+++ Extensibility/TemplatePack/ProjectTemplates/CSharp/Wpf/PrismMefApp/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Mef;
+using Prism.Mef;
+using $safeprojectname$.Views;
+using System.Windows;
+
+namespace $safeprojectname$
+{
+    class Bootstrapper : MefBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
