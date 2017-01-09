96ea2a1c3ff60c33ed269e12b2cd5ed5ef6e8412
nuits.jp <nuits.jp@live.jp>
2017-01-09 15:51:31 +0900

    Add project template for Prism.Autofac.Wpf

---
+++ Extensibility/TemplatePack/ProjectTemplates/CSharp/Wpf/PrismAutofacApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace $safeprojectname$.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Autofac Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
