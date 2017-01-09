41b9ac7ca5697e806d28a004a8b89e3122ea6213
nuits.jp <nuits.jp@live.jp>
2017-01-09 16:34:39 +0900

    Add project template for Prism.StructureMap

---
+++ Extensibility/TemplatePack/ProjectTemplates/CSharp/Wpf/PrismStructureMapApp/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.StructureMap;
+using Prism.StructureMap;
+using $safeprojectname$.Views;
+using System.Windows;
+
+namespace $safeprojectname$
+{
+    class Bootstrapper : StructureMapBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
