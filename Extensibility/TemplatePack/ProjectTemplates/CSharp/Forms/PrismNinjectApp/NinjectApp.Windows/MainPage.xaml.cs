5ddb55037db4f18f2e6114e54016dfbd6ea9ef52
nuits.jp <nuits.jp@live.jp>
2017-01-11 19:04:51 +0900

    Add Prism.Ninject.Forms project template

---
+++ Extensibility/TemplatePack/ProjectTemplates/CSharp/Forms/PrismNinjectApp/NinjectApp.Windows/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Ninject;
+using Prism.Ninject;
+
+namespace $safeprojectname$
+{
+    public sealed partial class MainPage 
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new $saferootprojectname$.App(new WindowsInitializer()));
+        }
+    }
+
+    public class WindowsInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IKernel kernel)
+        {
+
+        }
+    }
+}
