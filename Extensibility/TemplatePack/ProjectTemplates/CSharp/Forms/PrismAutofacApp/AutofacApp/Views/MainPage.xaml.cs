0aa58825f40393e2ee9206a7c5fdc43184d6e567
nuits.jp <nuits.jp@live.jp>
2017-01-23 16:39:13 +0900

    Add Assets.

---
+++ Extensibility/TemplatePack/ProjectTemplates/CSharp/Forms/PrismAutofacApp/AutofacApp/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace $safeprojectname$.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
