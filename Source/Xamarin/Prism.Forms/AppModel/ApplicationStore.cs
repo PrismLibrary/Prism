1142b64613d3fa756b1947885c637a6528f797be
Dan Siegel <me@dansiegel.net>
2017-03-13 11:23:20 -0700

    exposes `Application.Properties` and `Application.SavePropertiesAsync()`

---
+++ Source/Xamarin/Prism.Forms/AppModel/ApplicationStore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Prism.AppModel
+{
+    public class ApplicationStore : IApplicationStore
+    {
+        public IDictionary<string, object> Properties
+        {
+            get { return Application.Current.Properties; }
+        }
+
+        public Task SavePropertiesAsync() =>
+            Application.Current.SavePropertiesAsync();
+    }
+}
