a182a610bbb67ae89c095a55cea05bf84d011cc3
Viktor Hofer <viktor.hofer@outlook.com>
2017-03-14 19:57:50 +0100

    Fixed a lot of warnings: Not found rulesets, unused variables, verison conflicts

--- Source/Wpf/Prism.Unity.Wpf/Properties/AssemblyInfo.cs
+++ Source/Wpf/Prism.Unity.Wpf/Properties/AssemblyInfo.cs
@@ -16,7 +16,6 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: CLSCompliant(true)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
--- Source/Wpf/Prism.Wpf/Properties/AssemblyInfo.cs
+++ Source/Wpf/Prism.Wpf/Properties/AssemblyInfo.cs
@@ -22,8 +22,6 @@ using System.Windows.Markup;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-[assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguage("en")]
 
 // -----  Legacy -----
