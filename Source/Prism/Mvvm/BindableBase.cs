26b327c7f3448708ae143d37c160ceeffae14fe5
Brian Lagunas <brian.lagunas@live.com>
2017-03-22 09:57:28 -0600

    INPC method rename
    making API sonsistant with .NET coding standards and other Prism event
    methods

--- Source/Prism/Mvvm/BindableBase.cs
+++ Source/Prism/Mvvm/BindableBase.cs
@@ -1,5 +1,3 @@
-
-
 using System;
 using System.ComponentModel;
 using System.Linq.Expressions;
@@ -31,34 +29,96 @@ namespace Prism.Mvvm
         /// desired value.</returns>
         protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
         {
-            if (object.Equals(storage, value)) return false;
+            if (Equals(storage, value)) return false;
 
             storage = value;
-            this.OnPropertyChanged(propertyName);
+            RaisePropertyChanged(propertyName);
 
             return true;
         }
 
-        /// <summary>
-        /// Notifies listeners that a property value has changed.
-        /// </summary>
-        /// <param name="propertyName">Name of the property used to notify listeners. This
-        /// value is optional and can be provided automatically when invoked from compilers
-        /// that support <see cref="CallerMemberNameAttribute"/>.</param>
-        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+		/// <summary>
+		/// Checks if a property already matches a desired value. Sets the property and
+		/// notifies listeners only when necessary.
+		/// </summary>
+		/// <typeparam name="T">Type of the property.</typeparam>
+		/// <param name="storage">Reference to a property with both getter and setter.</param>
+		/// <param name="value">Desired value for the property.</param>
+		/// <param name="propertyName">Name of the property used to notify listeners. This
+		/// value is optional and can be provided automatically when invoked from compilers that
+		/// support CallerMemberName.</param>
+		/// <param name="onChanged">Action that is called after the property value has been changed.</param>
+		/// <returns>True if the value was changed, false if the existing value matched the
+		/// desired value.</returns>
+		protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null, Action onChanged = null)
+		{
+			if (Equals(storage, value)) return false;
+
+			storage = value;
+			onChanged?.Invoke();
+			RaisePropertyChanged(propertyName);
+
+			return true;
+		}
+
+		/// <summary>
+		/// Raises this object's PropertyChanged event.
+		/// </summary>
+		/// <param name="propertyName">Name of the property used to notify listeners. This
+		/// value is optional and can be provided automatically when invoked from compilers
+		/// that support <see cref="CallerMemberNameAttribute"/>.</param>
+		protected void RaisePropertyChanged([CallerMemberName]string propertyName = null)
+		{
+			//TODO: when we remove the old OnPropertyChanged method we need to uncomment the below line
+			//OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+			OnPropertyChanged(propertyName);
+		}
+
+		/// <summary>
+		/// Raises this object's PropertyChanged event for each of the properties.
+		/// </summary>
+		/// <param name="propertyNames">The properties that have a new value.</param>
+		protected void RaisePropertyChanged(params string[] propertyNames)
+		{
+			if (propertyNames == null) throw new ArgumentNullException(nameof(propertyNames));
+
+			foreach (var name in propertyNames)
+			{
+				RaisePropertyChanged(name);
+			}
+		}
+
+		/// <summary>
+		/// Notifies listeners that a property value has changed.
+		/// </summary>
+		/// <param name="propertyName">Name of the property used to notify listeners. This
+		/// value is optional and can be provided automatically when invoked from compilers
+		/// that support <see cref="CallerMemberNameAttribute"/>.</param>
+		[Obsolete("Please use the new RaisePropertyChanged method. This method will be removed to comply wth .NET coding standards. If you are overriding this method, you should overide the OnPropertyChanged(PropertyChangedEventArgs args) signature instead.")]
+		protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
         }
 
-        /// <summary>
-        /// Raises this object's PropertyChanged event.
-        /// </summary>
-        /// <typeparam name="T">The type of the property that has a new value</typeparam>
-        /// <param name="propertyExpression">A Lambda expression representing the property that has a new value.</param>
-        protected virtual void OnPropertyChanged<T>(Expression<Func<T>> propertyExpression)
+		/// <summary>
+		/// Raises this object's PropertyChanged event.
+		/// </summary>
+		/// <param name="args">The PropertyChangedEventArgs</param>
+		protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
+		{
+			PropertyChanged?.Invoke(this, args);
+		}
+
+		/// <summary>
+		/// Raises this object's PropertyChanged event.
+		/// </summary>
+		/// <typeparam name="T">The type of the property that has a new value</typeparam>
+		/// <param name="propertyExpression">A Lambda expression representing the property that has a new value.</param>
+		[Obsolete("Please use RaisePropertyChanged(nameof(PropertyName)) instead. Expressions are slower, and the new nameof feature eliminates the magic strings.")]
+		protected virtual void OnPropertyChanged<T>(Expression<Func<T>> propertyExpression)
         {
             var propertyName = PropertySupport.ExtractPropertyName(propertyExpression);
-            this.OnPropertyChanged(propertyName);
+            OnPropertyChanged(propertyName);
         }
     }
 }
