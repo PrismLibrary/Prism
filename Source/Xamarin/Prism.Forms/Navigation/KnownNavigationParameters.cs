6c5412888879ad9d86dc0c59b347e4a2b90ff90d
Dan Siegel <me@dansiegel.net>
2017-03-24 10:05:57 -0700

    allow users to dynamically create children and select a page from the query string

--- Source/Xamarin/Prism.Forms/Navigation/KnownNavigationParameters.cs
+++ Source/Xamarin/Prism.Forms/Navigation/KnownNavigationParameters.cs
@@ -3,5 +3,9 @@
     public static class KnownNavigationParameters
     {
         public const string NavigationMode = "__NavigationMode";
+
+        public const string CreateTab = "addTab";
+
+        public const string SelectedTab = "selectedTab";
     }
 }
