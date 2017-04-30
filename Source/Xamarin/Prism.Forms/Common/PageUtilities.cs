d3307b98ae7b6cffa80759cc2775e8e344690799
Dan Siegel <me@dansiegel.net>
2017-04-30 12:57:34 -0700

    c# 7 refractoring

--- Source/Xamarin/Prism.Forms/Common/PageUtilities.cs
+++ Source/Xamarin/Prism.Forms/Common/PageUtilities.cs
@@ -11,15 +11,12 @@ namespace Prism.Common
     {
         public static void InvokeViewAndViewModelAction<T>(object view, Action<T> action) where T : class
         {
-            T viewAsT = view as T;
-            if (viewAsT != null)
+            if (view is T viewAsT)
                 action(viewAsT);
 
-            var element = view as BindableObject;
-            if (element != null)
+            if (view is BindableObject element)
             {
-                var viewModelAsT = element.BindingContext as T;
-                if (viewModelAsT != null)
+                if (element.BindingContext is T viewModelAsT)
                 {
                     action(viewModelAsT);
                 }
@@ -45,29 +42,26 @@ namespace Prism.Common
 
         private static void DestroyChildren(Page page)
         {
-            if (page is MasterDetailPage)
+            if (page is MasterDetailPage mdp)
             {
-                DestroyPage(((MasterDetailPage)page).Detail);
+                DestroyPage(mdp.Detail);
             }
-            else if (page is TabbedPage)
+            else if (page is TabbedPage tabbedPage)
             {
-                var tabbedPage = (TabbedPage)page;
                 foreach (var item in tabbedPage.Children.Reverse())
                 {
                     DestroyPage(item);
                 }
             }
-            else if (page is CarouselPage)
+            else if (page is CarouselPage carouselPage)
             {
-                var carouselPage = (CarouselPage)page;
                 foreach (var item in carouselPage.Children.Reverse())
                 {
                     DestroyPage(item);
                 }
             }
-            else if (page is NavigationPage)
+            else if (page is NavigationPage navigationPage)
             {
-                var navigationPage = (NavigationPage)page;
                 foreach (var item in navigationPage.Navigation.NavigationStack.Reverse())
                 {
                     DestroyPage(item);
@@ -87,15 +81,12 @@ namespace Prism.Common
 
         public static Task<bool> CanNavigateAsync(object page, NavigationParameters parameters)
         {
-            var confirmNavigationItem = page as IConfirmNavigationAsync;
-            if (confirmNavigationItem != null)
+            if (page is IConfirmNavigationAsync confirmNavigationItem)
                 return confirmNavigationItem.CanNavigateAsync(parameters);
 
-            var bindableObject = page as BindableObject;
-            if (bindableObject != null)
+            if (page is BindableObject bindableObject)
             {
-                var confirmNavigationBindingContext = bindableObject.BindingContext as IConfirmNavigationAsync;
-                if (confirmNavigationBindingContext != null)
+                if (bindableObject.BindingContext is IConfirmNavigationAsync confirmNavigationBindingContext)
                     return confirmNavigationBindingContext.CanNavigateAsync(parameters);
             }
 
@@ -104,15 +95,12 @@ namespace Prism.Common
 
         public static bool CanNavigate(object page, NavigationParameters parameters)
         {
-            var confirmNavigationItem = page as IConfirmNavigation;
-            if (confirmNavigationItem != null)
+            if (page is IConfirmNavigation confirmNavigationItem)
                 return confirmNavigationItem.CanNavigate(parameters);
 
-            var bindableObject = page as BindableObject;
-            if (bindableObject != null)
+            if (page is BindableObject bindableObject)
             {
-                var confirmNavigationBindingContext = bindableObject.BindingContext as IConfirmNavigation;
-                if (confirmNavigationBindingContext != null)
+                if (bindableObject.BindingContext is IConfirmNavigation confirmNavigationBindingContext)
                     return confirmNavigationBindingContext.CanNavigate(parameters);
             }
 
@@ -165,12 +153,12 @@ namespace Prism.Common
         {
             Page child = null;
 
-            if (target is MasterDetailPage)
-                child = ((MasterDetailPage)target).Detail;
-            else if (target is TabbedPage)
-                child = ((TabbedPage)target).CurrentPage;
-            else if (target is CarouselPage)
-                child = ((CarouselPage)target).CurrentPage;
+            if (target is MasterDetailPage mdp)
+                child = mdp.Detail;
+            else if (target is TabbedPage tabbedPage)
+                child = tabbedPage.CurrentPage;
+            else if (target is CarouselPage carouselPage)
+                child = carouselPage.CurrentPage;
             else if (target is NavigationPage)
                 child = target.Navigation.NavigationStack.Last();
 
@@ -180,7 +168,7 @@ namespace Prism.Common
             return target;
         }
 
-        public static Page GetPreviousPage(Page currentPage, System.Collections.Generic.IReadOnlyList<Page> navStack)
+        public static Page GetPreviousPage(Page currentPage, IReadOnlyList<Page> navStack)
         {
             Page previousPage = null;
 
@@ -192,7 +180,7 @@ namespace Prism.Common
             return previousPage;
         }
 
-        public static int GetCurrentPageIndex(Page currentPage, System.Collections.Generic.IReadOnlyList<Page> navStack)
+        public static int GetCurrentPageIndex(Page currentPage, IReadOnlyList<Page> navStack)
         {
             int stackCount = navStack.Count;
             for (int x = 0; x < stackCount; x++)
