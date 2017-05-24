73e7435242e52329b56a20d7e9eaa09576fc77cc
Dan Siegel <me@dansiegel.net>
2017-05-23 20:24:13 -0700

    updating AssemblyInfo

--- Source/Wpf/Prism.Wpf/Properties/AssemblyInfo.cs
+++ Source/Wpf/Prism.Wpf/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.3")]
-[assembly: AssemblyFileVersion("6.3.0")]
-[assembly: AssemblyInformationalVersion("6.3.0")]
+[assembly: AssemblyVersion("7.0")]
+[assembly: AssemblyFileVersion("7.0.0")]
+[assembly: AssemblyInformationalVersion("7.0.0")]
