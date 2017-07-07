a13a2c31b23d31b6a0eaef8b7f3336e405875a0d
Brian Lagunas <brian.lagunas@live.com>
2017-07-07 12:14:53 -0600

    fixed debuglogger

--- Source/Prism/Logging/DebugLogger.cs
+++ Source/Prism/Logging/DebugLogger.cs
@@ -1,10 +1,9 @@
-﻿using System;
+﻿// Define DEBUG symbol to enable Debug.WriteLine to work even when Prism is compiled in Release-mode for Nuget-feed.
+#define DEBUG
+using System;
 using System.Diagnostics;
 using System.Globalization;
 using Prism.Properties;
-
-// Define DEBUG symbol to enable Debug.WriteLine to work even when Prism is compiled in Release-mode for Nuget-feed.
-#define DEBUG
 
 namespace Prism.Logging
 {
