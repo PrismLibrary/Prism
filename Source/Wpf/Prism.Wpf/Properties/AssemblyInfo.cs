3bd08b62d9f74e9e3493fab2b6833f9c012df0e4
Dan Siegel <me@dansiegel.net>
2017-11-09 10:28:38 -0800

    update WPF Version for CI Builds

--- Source/Wpf/Prism.Wpf/Properties/AssemblyInfo.cs
+++ Source/Wpf/Prism.Wpf/Properties/AssemblyInfo.cs
@@ -40,16 +40,4 @@ using System.Windows.Markup;
 [assembly: XmlnsDefinition("http://prismlibrary.com/", "Prism.Interactivity")]
 [assembly: XmlnsDefinition("http://prismlibrary.com/", "Prism.Interactivity.InteractionRequest")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.0")]
-[assembly: AssemblyFileVersion("7.0.0")]
-[assembly: AssemblyInformationalVersion("7.0.0")]
+
