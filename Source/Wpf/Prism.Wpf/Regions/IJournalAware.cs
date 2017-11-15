ee9ffbb60f74c6422d7e99eea71fc42380275e8e
Brian Lagunas <brian.lagunas@live.com>
2017-11-14 18:09:57 -0700

    refactored and added comments

--- Source/Wpf/Prism.Wpf/Regions/IJournalAware.cs
+++ Source/Wpf/Prism.Wpf/Regions/IJournalAware.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Prism.Regions
+﻿namespace Prism.Regions
 {
+    /// <summary>
+    /// Provides a way for objects involved in navigation to opt-out of being added to the IRegionNavigationJournal backstack.
+    /// </summary>
     public interface IJournalAware
     {
+        /// <summary>
+        /// Determines if the current obect is going to be added to the navigation journal's backstack.
+        /// </summary>
+        /// <returns>True, add to backstack. False, remove from backstack.</returns>
         bool PersistInHistory();
     }
 }
