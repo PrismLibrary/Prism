cd6457cb55b7cf9cc71247d89ae699f93b90f58c
Dan Siegel <me@dansiegel.net>
2017-11-24 15:42:46 -0800

    Update Ninject for Container Abstractions

---
+++ Source/Xamarin/Prism.Ninject.Forms/NinjectContainerExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ninject;
+using Prism.Ioc;
+
+namespace Prism.Ninject
+{
+    public class NinjectContainerExtension : IContainerExtension<IKernel>
+    {
+        public NinjectContainerExtension(IKernel kernel)
+        {
+            Instance = kernel;
+        }
+
+        public IKernel Instance { get; }
+    }
+}
