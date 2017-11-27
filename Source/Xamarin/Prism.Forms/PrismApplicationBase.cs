fd98782c673b485bfa82d32c60a87df22cb6c131
Brian Lagunas <brian.lagunas@live.com>
2017-11-27 11:22:01 -0700

    Revert "registered provider with container"
    This reverts commit 0886e91f2d480b1693ee853bca388676e5624f89.

--- Source/Xamarin/Prism.Forms/PrismApplicationBase.cs
+++ Source/Xamarin/Prism.Forms/PrismApplicationBase.cs
@@ -100,7 +100,6 @@ namespace Prism
         protected virtual void ConfigureContainer(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterInstance<IContainerExtension>(_containerExtension);
-            containerRegistry.RegisterInstance<IContainerProvider>(_containerExtension);
             containerRegistry.RegisterSingleton<ILoggerFacade, EmptyLogger>();
             containerRegistry.RegisterSingleton<IApplicationProvider, ApplicationProvider>();
             containerRegistry.RegisterSingleton<IApplicationStore, ApplicationStore>();
