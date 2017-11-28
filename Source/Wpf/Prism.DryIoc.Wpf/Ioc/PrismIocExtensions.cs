f309ac27e3cefbdcc7e162311fbbe6a6ca399843
Brian Lagunas <brian.lagunas@live.com>
2017-11-28 12:45:49 -0700

    added DryIoc PrismApplication

---
+++ Source/Wpf/Prism.DryIoc.Wpf/Ioc/PrismIocExtensions.cs
@@ -0,0 +1,18 @@
+﻿using DryIoc;
+using Prism.Ioc;
+
+namespace Prism.DryIoc
+{
+    public static class PrismIocExtensions
+    {
+        public static IContainer GetInstance(this IContainerProvider containerProvider)
+        {
+            return ((IContainerExtension<IContainer>)containerProvider).Instance;
+        }
+
+        public static IContainer GetInstance(this IContainerRegistry containerRegistry)
+        {
+            return ((IContainerExtension<IContainer>)containerRegistry).Instance;
+        }
+    }
+}
