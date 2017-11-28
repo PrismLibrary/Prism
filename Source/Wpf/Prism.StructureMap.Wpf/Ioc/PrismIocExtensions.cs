9a7604b905c1eafd820274f203ce9df18e29b23a
Brian Lagunas <brian.lagunas@live.com>
2017-11-28 13:17:38 -0700

    added StructureMap extension

---
+++ Source/Wpf/Prism.StructureMap.Wpf/Ioc/PrismIocExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using StructureMap;
+
+namespace Prism.StructureMap
+{
+    public static class PrismIocExtensions
+    {
+        public static IContainer GetInstance(this IContainerProvider containerProvider)
+        {
+            return ((IContainerExtension<IContainer>)containerProvider).Instance;
+        }
+
+        public static IContainer GetInstance(this IContainerRegistry containerRegistry)
+        {
+            return ((IContainerExtension<IContainer>)containerRegistry).Instance;
+        }
+    }
+}
