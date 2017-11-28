b9ae25ca0a1d09cbcd57a0bf1a02bd5363823f23
Brian Lagunas <brian.lagunas@live.com>
2017-11-28 12:59:33 -0700

    added Autofac PrismApplication

---
+++ Source/Wpf/Prism.Autofac.Wpf/Ioc/PrismIocExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Prism.Ioc;
+
+namespace Prism.Autofac.Ioc
+{
+    public static class PrismIocExtensions
+    {
+        public static IContainer GetInstance(this IContainerProvider containerProvider)
+        {
+            return ((IContainerExtension<IContainer>)containerProvider).Instance;
+        }
+
+        public static IContainer GetInstance(this IContainerRegistry containerRegistry)
+        {
+            return ((IContainerExtension<IContainer>)containerRegistry).Instance;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="ContainerBuilder"/> used to register services.
+        /// </summary>
+        /// <param name="registry"></param>
+        /// <returns>The current <see cref="ContainerBuilder"/></returns>
+        public static ContainerBuilder GetBuilder(this IContainerRegistry registry)
+        {
+            return ((IAutofacContainerExtension)registry).Builder;
+        }
+    }
+}
