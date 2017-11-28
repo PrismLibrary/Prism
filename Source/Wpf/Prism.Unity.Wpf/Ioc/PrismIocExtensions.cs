e5f4764867da4a3cf76758335ddc1d4bc1c5c960
Brian Lagunas <brian.lagunas@live.com>
2017-11-28 12:22:50 -0700

    added new PrismApplication class

---
+++ Source/Wpf/Prism.Unity.Wpf/Ioc/PrismIocExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using Unity;
+
+namespace Prism.Unity
+{
+    public static class PrismIocExtensions
+    {
+        public static IUnityContainer GetInstance(this IContainerProvider containerProvider)
+        {
+            return ((IContainerExtension<IUnityContainer>)containerProvider).Instance;
+        }
+
+        public static IUnityContainer GetInstance(this IContainerRegistry containerRegistry)
+        {
+            return ((IContainerExtension<IUnityContainer>)containerRegistry).Instance;
+        }
+    }
+}
