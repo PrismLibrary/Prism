0dffa0931bdeb3d5ed6f5c8b87ff37ede6e0f05f
Brian Lagunas <brian.lagunas@live.com>
2018-02-28 19:12:24 -0700

    added clarifying comments

--- Source/Prism/Navigation/INavigatedAwareAsync.cs
+++ Source/Prism/Navigation/INavigatedAwareAsync.cs
@@ -5,11 +5,13 @@ namespace Prism.Navigation
     /// <summary>
     /// Provides a way for ViewModels involved in navigation to be notified of navigation activities after the target Page has been added to the navigation stack.
     /// </summary>
+    /// <remarks>Not currently supported on Xamarin.Forms</remarks>
     public interface INavigatedAwareAsync
     {
         /// <summary>
         /// Called when the implementer has been navigated to.
         /// </summary>
+        /// <remarks>Not currently supported on Xamarin.Forms</remarks>
         /// <param name="parameters">The navigation parameters.</param>
         Task OnNavigatedToAsync(INavigationParameters parameters);
     }
