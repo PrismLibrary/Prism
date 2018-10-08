fab4fadcfc32a5d656ac86a6f8cc52d4d9548d15
Alan <github.com@kyro.co.za>
2018-10-08 21:16:19 +0100

    Corrected the ViewModelLocationProvider's comments
    Fixed minor typing errors in the ViewModelLocationProvider class file.

--- Source/Prism/Mvvm/ViewModelLocationProvider.cs
+++ Source/Prism/Mvvm/ViewModelLocationProvider.cs
@@ -9,9 +9,9 @@ namespace Prism.Mvvm
 {
     /// <summary>
     /// The ViewModelLocationProvider class locates the view model for the view that has the AutoWireViewModelChanged attached property set to true.
-    /// The view model will be located and injected into the view's DataContext. To locate the view, two strategies are used: First the ViewModelLocationProvider
+    /// The view model will be located and injected into the view's DataContext. To locate the view model, two strategies are used: First the ViewModelLocationProvider
     /// will look to see if there is a view model factory registered for that view, if not it will try to infer the view model using a convention based approach.
-    /// This class also provide methods for registering the view model factories,
+    /// This class also provides methods for registering the view model factories,
     /// and also to override the default view model factory and the default view type to view model type resolver.
     /// </summary>
 
