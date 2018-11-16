4e6c1b47b88b2511bb90f5341a15c6da535fbc2f
Allan Ritchie <allan.ritchie@gmail.com>
2018-11-16 07:30:15 -0500

    add service applink service mock

---
+++ Source/Xamarin/Prism.Forms.Tests/Mocks/AppLinkServiceMock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Prism.Services;
+using Xamarin.Forms;
+
+namespace Prism.Forms.Tests.Mocks
+{
+    public class AppLinkServiceMock : IAppLinkService
+    {
+        public List<IAppLinkEntry> Entries { get; set; } = new List<IAppLinkEntry>();
+
+        public void RegisterLink(IAppLinkEntry entry)
+        {
+            this.Entries.Add(entry);
+        }
+
+        public void DeregisterLink(IAppLinkEntry entry)
+        {
+            this.Entries.Remove(entry);
+        }
+
+        public void DeregisterLink(Uri uri)
+        {
+            var link = this.Entries.FirstOrDefault(x => x.AppLinkUri.Equals(uri));
+            if (link != null)
+                this.Entries.Remove(link);
+        }
+    }
+}
