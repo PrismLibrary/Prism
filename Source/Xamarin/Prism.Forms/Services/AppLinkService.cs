33f1ea94cf5c1fe4d7edd99abd3571e645ac9303
Allan Ritchie <allan.ritchie@gmail.com>
2018-11-16 07:30:05 -0500

    add access to XF applinks registry

---
+++ Source/Xamarin/Prism.Forms/Services/AppLinkService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Prism.Services
+{
+    public class AppLinkService : IAppLinkService
+    {
+        public void RegisterLink(IAppLinkEntry entry)
+        {
+            Application.Current.AppLinks.RegisterLink(entry);
+        }
+
+        public void DeregisterLink(IAppLinkEntry entry)
+        {
+            Application.Current.AppLinks.DeregisterLink(entry);
+        }
+
+        public void DeregisterLink(Uri uri)
+        {
+            Application.Current.AppLinks.DeregisterLink(uri);
+        }
+    }
+}
---
+++ Source/Xamarin/Prism.Forms/Services/IAppLinkService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Prism.Services
+{
+    public interface IAppLinkService
+    {
+        /// <summary>
+        /// Add the provided application link to the application index
+        /// </summary>
+        /// <param name="entry">Application link details</param>
+        void RegisterLink(IAppLinkEntry entry);
+
+        /// <summary>
+        /// Removes the provided application link from the application index
+        /// </summary>
+        /// <param name="entry">Application link details</param>
+        void DeregisterLink(IAppLinkEntry entry);
+
+        /// <summary>
+        /// Removes the provided application link from the application index
+        /// </summary>
+        /// <param name="uri">The URI of your registered application link</param>
+        void DeregisterLink(Uri uri);
+    }
+}
