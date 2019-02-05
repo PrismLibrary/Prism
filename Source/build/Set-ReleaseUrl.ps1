e9e27fb7e1d1b1828559f25bab04654013e2c68d
Dan Siegel <me@dansiegel.net>
2019-02-04 18:32:48 -0800

    fixing release URL

--- Source/build/Set-ReleaseUrl.ps1
+++ Source/build/Set-ReleaseUrl.ps1
@@ -1,12 +1,13 @@
+$PackageReleaseNotes = "https://github.com/PrismLibrary/Prism/releases/tag/v7.2.0.$env:BUILD_BUILDID"
+
 if($env:IS_RELEASE)
 {
-    $PackageReleaseNotes = "https://github.com/PrismLibrary/Prism/releases/tag/v7.2.0.$env:IS_RELEASE"
     Write-Host "Package Release Notes Url set to: $PackageReleaseNotes"
     Write-Output ("##vso[task.setvariable variable=PackageReleaseNotes;]$PackageReleaseNotes")
 }
 elseif ($env:IS_PREVIEW)
 {
-    $PackageReleaseNotes = "https://github.com/PrismLibrary/Prism/releases/tag/v7.2.0.$env:IS_PREVIEW-pre"
+    $PackageReleaseNotes = "$PackageReleaseNotes-pre"
     Write-Host "Package Release Notes Url set to: $PackageReleaseNotes"
     Write-Output ("##vso[task.setvariable variable=PackageReleaseNotes;]$PackageReleaseNotes")
 }
