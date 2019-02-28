5b8dc47e5e99abd5761165dca0119ff0e1763091
Bart Lannoeye <bart.lannoeye@gmail.com>
2019-02-28 16:18:52 +0100

    Add empty UWP test project

---
+++ Source/Windows10/Prism.Windows.Tests/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Prism.Windows.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void AssertTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
