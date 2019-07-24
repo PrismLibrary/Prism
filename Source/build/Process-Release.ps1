ae8135adf964b13e635f984e4ddf90c7bdf1743a
Dan Siegel <me@dansiegel.net>
2019-07-24 07:28:32 -0700

    move ps script to ps1

--- Source/build/Process-Release.ps1
+++ Source/build/Process-Release.ps1
@@ -1,12 +1,43 @@
-$nupkg = Get-ChildItem -Path $env:PIPELINE_WORKSPACE -Filter *.nupkg -Recurse | Select-Object -First 1
-$nupkg.Name -match '^(.*?)\.((?:\.?[0-9]+){3,}(?:[-a-z]+)?)\.nupkg$'
+try {
+    $artifactDirectory = Join-Path -Path $env:PIPELINE_WORKSPACE -ChildPath 'NuGet'
 
-$VersionName = $Matches[2]
-$artifacts = "$env:PIPELINE_WORKSPACE/Release"
+    Write-Host "Currect working directory - $artifactDirectory"
 
-Write-Host "Version Name" $VersionName
-Write-Host "DLL Artifacts: $artifacts"
+    $nupkg = Get-ChildItem -Path $artifactDirectory -Filter *.nupkg -Recurse | Select-Object -First 1
 
-Write-Output ("##vso[task.setvariable variable=VersionName;]$VersionName")
-Write-Output ("##vso[task.setvariable variable=DLLArtifactsPath;]$artifacts")
-Write-Output ("##vso[task.setvariable variable=DLLArtifactsZip;]$artifacts.zip")
+    if($null -eq $nupkg)
+    {
+        Throw "No NuGet Package could be found in the current directory"
+    }
+
+    Write-Host "Package Name $($nupkg.Name)"
+    $nupkg.Name -match '^(.*?)\.((?:\.?[0-9]+){3,}(?:[-a-z]+)?)\.nupkg$'
+    $VersionName = $Matches[2]
+    $IsPreview = $VersionName -match '-pre$'
+    $ReleaseDisplayName = $VersionName
+    $artifacts = "$env:PIPELINE_WORKSPACE/Release"
+
+    if($null -eq $env:IS_PREVIEW)
+    {
+        Write-Output ("##vso[task.setvariable variable=IS_PREVIEW;]$IsPreview")
+    }
+
+    if($true -eq $IsPreview)
+    {
+        $baseVersion = $VersionName.Split('-')[0]
+        $ReleaseDisplayName = "$baseVersion - Preview"
+    }
+
+    Write-Host "Version Name - $VersionName"
+    Write-Host "Release Display Name - $ReleaseDisplayName"
+    Write-Host "DLL Artifacts - $artifacts"
+
+    Write-Output ("##vso[task.setvariable variable=VersionName;]$VersionName")
+    Write-Output ("##vso[task.setvariable variable=ReleaseDisplayName;]$ReleaseDisplayName")
+    Write-Output ("##vso[task.setvariable variable=DLLArtifactsPath;]$artifacts")
+    Write-Output ("##vso[task.setvariable variable=DLLArtifactsZip;]$artifacts.zip")
+}
+catch {
+    Write-Error  $_
+    exit 1
+}
