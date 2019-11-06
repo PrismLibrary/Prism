c9a11d998e17c7f3f2d4eb57749064690bd9aea2
Amr <hassaninamr@yahoo.com>
2019-11-06 11:44:40 +0200

    Update PrismApplicationBase.cs

--- Source/Xamarin/Prism.Forms/PrismApplicationBase.cs
+++ Source/Xamarin/Prism.Forms/PrismApplicationBase.cs
@@ -230,16 +230,20 @@ namespace Prism
 
         protected override void OnResume()
         {
-            if (MainPage == null) return;
-            var page = PageUtilities.GetCurrentPage(MainPage);
-            PageUtilities.InvokeViewAndViewModelAction<IApplicationLifecycleAware>(page, x => x.OnResume());
+            if (MainPage != null)
+            {
+                var page = PageUtilities.GetCurrentPage(MainPage);
+                PageUtilities.InvokeViewAndViewModelAction<IApplicationLifecycleAware>(page, x => x.OnResume());
+            }
         }
 
         protected override void OnSleep()
         {
-            if (MainPage == null) return;
-            var page = PageUtilities.GetCurrentPage(MainPage);
-            PageUtilities.InvokeViewAndViewModelAction<IApplicationLifecycleAware>(page, x => x.OnSleep());
+            if (MainPage != null)
+            {
+                var page = PageUtilities.GetCurrentPage(MainPage);
+                PageUtilities.InvokeViewAndViewModelAction<IApplicationLifecycleAware>(page, x => x.OnSleep());
+            }
         }
 
         private void PrismApplicationBase_ModalPopping(object sender, ModalPoppingEventArgs e)
