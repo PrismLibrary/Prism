a1aa91c3006525f7f1f7b3b45796e6506edc67f6
John Hermsen <j.hermsen@antsolutions.nl>
2019-11-27 13:30:39 +0100

    Ensure Binding Context on Parameter refresh/update when using MarkupExtension, fixes #1917

--- Source/Xamarin/Prism.Forms/Xaml/ParameterExtension.cs
+++ Source/Xamarin/Prism.Forms/Xaml/ParameterExtension.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Xaml.Internals;
 
 namespace Prism.Xaml
 {
     public class ParameterExtension : Parameter, IMarkupExtension<Parameter>
     {
-        public Parameter ProvideValue(IServiceProvider serviceProvider) =>
-            this;
+        public Parameter ProvideValue(IServiceProvider serviceProvider)
+        {
+            var target = serviceProvider.GetService<IProvideValueTarget>();
+            if (target != null && target.TargetObject is BindableObject bindableTargetObject)
+            {
+                var self = this;
+                bindableTargetObject.BindingContextChanged -= BindingContextChanged;
+                bindableTargetObject.BindingContextChanged += BindingContextChanged;
+
+                void BindingContextChanged(object parentObject, EventArgs args)
+                {
+                    var parent = (BindableObject)parentObject;
+                    self.BindingContext = parent?.BindingContext;
+                }
+            }
+        
+            return this;
+        }
 
         object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider) =>
             ProvideValue(serviceProvider);
