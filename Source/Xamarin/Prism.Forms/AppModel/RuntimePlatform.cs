1a9ad651aba637db33c40f5036d6b130cf07c672
Dan Siegel <me@dansiegel.net>
2019-12-13 13:25:14 -0800

    Removing deprecated platforms, Adding new Platforms - fixes #1962

--- Source/Xamarin/Prism.Forms/AppModel/RuntimePlatform.cs
+++ Source/Xamarin/Prism.Forms/AppModel/RuntimePlatform.cs
@@ -7,12 +7,12 @@
     public enum RuntimePlatform
     {
         Android,
+        GTK,
         iOS,
         macOS,
         Tizen,
         UWP,
-        WinPhone,
-        WinRT,
+        WPF,
         Unknown
     }
 }
