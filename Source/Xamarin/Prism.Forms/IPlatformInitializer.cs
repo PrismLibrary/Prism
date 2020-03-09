ae96b42322cbbaf9ef5bdd74b7abd6c684970eed
Geoffrey Dudics <gdudics@gmail.com>
2020-03-08 22:38:54 -0400

    Added documentation to IPlatformInitializer

--- Source/Xamarin/Prism.Forms/IPlatformInitializer.cs
+++ Source/Xamarin/Prism.Forms/IPlatformInitializer.cs
@@ -2,8 +2,22 @@
 
 namespace Prism
 {
+    /// <summary>
+    /// IPlatformInitializer Interface
+    /// </summary>
+    /// <example> 
+    /// IPlatformInitializer is used when there are specific types that need to be registered on a platform.
+    /// </example>
     public interface IPlatformInitializer
     {
+        /// <summary>
+		/// Registers platform specific types
+		/// </summary>
+        /// <example> 
+        /// <see cref="RegisterTypes(IContainerRegistry)"/>
+        /// <code>
+        /// </code>
+        /// </example>
         void RegisterTypes(IContainerRegistry containerRegistry);
     }
 }
