b1b8707455919caa3710d272556e0151aa82694e
Dan Siegel <me@dansiegel.net>
2020-04-02 07:19:41 -0700

    project restructure

---
+++ e2e/Forms/src/HelloWorld.Android/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace HelloWorld.Droid
+{
+    [Activity(
+        Theme = "@style/MainTheme",
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App(new AndroidInitializer()));
+        }
+    }
+}
+
