eba083ee046857b7645189f87da303c0ea97fcd8
Brian Lagunas <brianlagunas@users.noreply.github.com>
2020-04-21 11:23:06 -0600

    Added new PrismBootstrapper API

---
+++ e2e/Wpf/HelloWorld.Bootstraper/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using HelloWorld.Views;
+using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+using Unity;
+
+namespace HelloWorld
+{
+    class Bootstrapper : PrismBootstrapper
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
---
+++ src/Wpf/Prism.DryIoc.Wpf/PrismBootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using DryIoc;
+using Prism.Ioc;
+using System;
+
+namespace Prism.DryIoc
+{
+    public abstract class PrismBootstrapper : PrismBootstrapperBase
+    {
+        /// <summary>
+        /// Create <see cref="Rules" /> to alter behavior of <see cref="IContainer" />
+        /// </summary>
+        /// <returns>An instance of <see cref="Rules" /></returns>
+        protected virtual Rules CreateContainerRules() => Rules.Default.WithAutoConcreteTypeResolution()
+                                                                       .With(Made.Of(FactoryMethod.ConstructorWithResolvableArguments))
+                                                                       .WithDefaultIfAlreadyRegistered(IfAlreadyRegistered.Replace);
+
+        protected override IContainerExtension CreateContainerExtension()
+        {
+            return new DryIocContainerExtension(new Container(CreateContainerRules()));
+        }
+
+        protected override void RegisterFrameworkExceptionTypes()
+        {
+            ExceptionExtensions.RegisterFrameworkExceptionType(typeof(ContainerException));
+        }
+    }
+}
