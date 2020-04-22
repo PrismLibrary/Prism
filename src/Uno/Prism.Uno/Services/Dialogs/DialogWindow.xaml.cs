a63b29fdf90dfb1a1aa6f28cb0259c122126bc82
Jerome Laban <jerome.laban@nventive.com>
2020-04-21 22:06:26 -0400

    Add Uno Default implementation for IDialogWindow

---
+++ src/Uno/Prism.Uno/Services/Dialogs/DialogWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Prism.Services.Dialogs
+{
+    /// <summary>
+    /// Prism's default dialog host.
+    /// </summary>
+    public partial class DialogWindow : ContentDialog, IDialogWindow
+    {
+        public DialogWindow()
+        {
+            this.InitializeComponent();
+        }
+        public IDialogResult Result { get; set; }
+
+        event RoutedEventHandler IDialogWindow.Loaded
+        {
+            add => Loaded += value;
+            remove => Loaded -= value;
+        }
+
+        event TypedEventHandler<ContentDialog, ContentDialogClosingEventArgs> IDialogWindow.Closing
+        {
+            add => Closing += value;
+            remove => Closing -= value;
+        }
+
+        event TypedEventHandler<ContentDialog, ContentDialogClosedEventArgs> IDialogWindow.Closed
+        {
+            add => Closed += value;
+            remove => Closed -= value;
+        }
+    }
+}
