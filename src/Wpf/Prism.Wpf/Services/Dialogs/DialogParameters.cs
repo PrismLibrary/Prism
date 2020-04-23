b14cac84dff039eed8e4cafcb87c51b81f0da7f5
Brian Lagunas <brianlagunas@users.noreply.github.com>
2020-04-23 11:38:40 -0600

    refactored parameter classes to use ParametersBase class

--- src/Wpf/Prism.Wpf/Services/Dialogs/DialogParameters.cs
+++ src/Wpf/Prism.Wpf/Services/Dialogs/DialogParameters.cs
@@ -1,11 +1,24 @@
-﻿using Prism.Regions;
+﻿using Prism.Common;
 
 namespace Prism.Services.Dialogs
 {
-    public class DialogParameters : NavigationParameters, IDialogParameters
+    /// <summary>
+    /// Represents Dialog parameters.
+    /// </summary>
+    /// <remarks>
+    /// This class can be used to to pass object parameters during the showing and closing of Dialogs.
+    /// </remarks>
+    public class DialogParameters : ParametersBase, IDialogParameters
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DialogParameters"/> class.
+        /// </summary>
         public DialogParameters() : base() { }
 
+        /// <summary>
+        /// Constructs a list of parameters
+        /// </summary>
+        /// <param name="query">Query string to be parsed</param>
         public DialogParameters(string query) : base(query) { }
     }
 }
