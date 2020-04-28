72f1a35dfd5c5876054194e73473b3b99ddcf81c
Geoffrey Dudics <gdudics@gmail.com>
2020-04-28 10:37:26 -0400

    Added documenation for: ApplicationStore.cs EventToCommandBehavior.cs IApplicationLifecycleAware.cs IApplicationStore.cs RuntimePlatform.cs

--- src/Forms/Prism.Forms/AppModel/IApplicationLifecycleAware.cs
+++ src/Forms/Prism.Forms/AppModel/IApplicationLifecycleAware.cs
@@ -1,9 +1,18 @@
 ﻿namespace Prism.AppModel
 {
+    /// <summary>
+    /// Interface to handle OS related events when Application is put to sleep, etc.
+    /// </summary>
     public interface IApplicationLifecycleAware
     {
+        /// <summary>
+        /// Called when application is resumed
+        /// </summary>
         void OnResume();
 
+        /// <summary>
+        /// Called when application is put to sleep
+        /// </summary>
         void OnSleep();
     }
 }
--- src/Forms/Prism.Forms/AppModel/RuntimePlatform.cs
+++ src/Forms/Prism.Forms/AppModel/RuntimePlatform.cs
@@ -6,13 +6,44 @@
     /// <remarks>This enum acts as a wrapper around the Device.RuntimePlatform string-based options</remarks>
     public enum RuntimePlatform
     {
+        /// <summary>
+        /// Google Android
+        /// </summary>
         Android,
+
+        /// <summary>
+        /// GTK UI projects
+        /// </summary>
         GTK,
+
+        /// <summary>
+        /// Apple IOS
+        /// </summary>
         iOS,
+
+        /// <summary>
+        /// Apple macOS
+        /// </summary>
         macOS,
+
+        /// <summary>
+        /// Tizen OS
+        /// </summary>
         Tizen,
+
+        /// <summary>
+        /// Universal Windows Platform
+        /// </summary>
         UWP,
+
+        /// <summary>
+        /// Windows Presentation Foundation
+        /// </summary>
         WPF,
+
+        /// <summary>
+        /// Catchall for unlisted Platform
+        /// </summary>
         Unknown
     }
 }
