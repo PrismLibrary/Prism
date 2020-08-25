3edc501c362ce321fc127047be8226b7baff7e5d
Dan Siegel <me@dansiegel.net>
2020-08-25 08:54:10 -0700

    fix iOS modal transparency

--- src/Forms/Prism.Forms/Services/Dialogs/DialogPage.cs
+++ src/Forms/Prism.Forms/Services/Dialogs/DialogPage.cs
@@ -1,4 +1,6 @@
 ﻿using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace Prism.Services.Dialogs
 {
@@ -8,6 +10,7 @@ namespace Prism.Services.Dialogs
         {
             AutomationId = "PrismDialogModal";
             BackgroundColor = Color.Transparent;
+            On<iOS>().SetModalPresentationStyle(UIModalPresentationStyle.OverFullScreen);
         }
 
         public View DialogView { get; set; }
