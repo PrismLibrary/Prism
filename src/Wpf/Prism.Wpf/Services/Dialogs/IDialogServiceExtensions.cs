6526c6eaa31c019c7b415b92403264fae8382f76
Brian Lagunas <brianlagunas@users.noreply.github.com>
2020-09-03 13:45:49 -0600

    added more overrides

--- src/Wpf/Prism.Wpf/Services/Dialogs/IDialogServiceExtensions.cs
+++ src/Wpf/Prism.Wpf/Services/Dialogs/IDialogServiceExtensions.cs
@@ -13,12 +13,32 @@ namespace Prism.Services.Dialogs
         /// </summary>
         /// <param name="dialogService">The DialogService</param>
         /// <param name="name">The name of the dialog to show.</param>
+        public static void Show(this IDialogService dialogService, string name)
+        {
+            dialogService.Show(name, new DialogParameters(), null);
+        }
+
+        /// <summary>
+        /// Shows a non-modal dialog.
+        /// </summary>
+        /// <param name="dialogService">The DialogService</param>
+        /// <param name="name">The name of the dialog to show.</param>
         /// <param name="callback">The action to perform when the dialog is closed.</param>
         public static void Show(this IDialogService dialogService, string name, Action<IDialogResult> callback)
         {
             dialogService.Show(name, new DialogParameters(), callback);
         }
 #endif
+        /// <summary>
+        /// Shows a modal dialog.
+        /// </summary>
+        /// <param name="dialogService">The DialogService</param>
+        /// <param name="name">The name of the dialog to show.</param>
+        public static void ShowDialog(this IDialogService dialogService, string name)
+        {
+            dialogService.ShowDialog(name, new DialogParameters(), null);
+        }
+
         /// <summary>
         /// Shows a modal dialog.
         /// </summary>
