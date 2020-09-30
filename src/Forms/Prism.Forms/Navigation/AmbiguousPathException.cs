292ada95908d105f7ca9ef28ea8fa50068c4ce20
Vincent Niehues <vniehues@icloud.com>
2020-09-30 10:23:33 +0200

    AmbiguousPathException is now in a separate file; PageNavigationService spacing & indentation fixed;

---
+++ src/Forms/Prism.Forms/Navigation/AmbiguousPathException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Prism.Navigation
+{
+    public class AmbiguousPathException : Exception
+    {
+        public const string AmbiguousSelectedTab = "The requested selected tab matches more than one child page";
+
+        public string Name { get; }
+
+        public AmbiguousPathException(string name)
+        {
+            Name = name;
+        }
+    }
+}
