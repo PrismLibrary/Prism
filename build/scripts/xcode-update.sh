bce73e17ede0bcfe7021e83ead70431b5a7f0a0a
Jérôme Laban <jerome.laban@nventive.com>
2020-10-29 12:08:12 -0400

    ci: Apply temporary workaround for https://github.com/actions/virtual-environments/issues/1932

--- build/scripts/xcode-update.sh
+++ build/scripts/xcode-update.sh
@@ -4,3 +4,6 @@ echo "Setting Xcode Override to: $xcodeRoot"
 
 echo '##vso[task.setvariable variable=MD_APPLE_SDK_ROOT;]'$xcodeRoot
 sudo xcode-select --switch $xcodeRoot/Contents/Developer
+
+# Apply temporary workaround for https://github.com/actions/virtual-environments/issues/1932
+rm -f ${HOME}/Library/Preferences/Xamarin/Settings.plist
