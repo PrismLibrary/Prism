9f0f4512bb0b671c8292344d762d0302706a6baa
Dan Siegel <me@dansiegel.net>
2021-01-27 09:34:07 -0800

    remove legacy values from Prism.UWP

--- src/Forms/Prism.Forms/Navigation/NavigationMode.cs
+++ src/Forms/Prism.Forms/Navigation/NavigationMode.cs
@@ -13,15 +13,5 @@
         /// Indicates that a new navigation operation has occured and a new page has been added to the navigation stack.
         /// </summary>
         New,
-        /// <summary>
-        /// Indicates that a forward navigation operation has occured to an existing page.
-        /// </summary>
-        /// <remarks>Not currently supported on Xamarin.Forms</remarks>
-        Forward,
-        /// <summary>
-        /// Indicates that the current page in the navigation stack has been navigated to again, or it's state has been refreshed.
-        /// </summary>
-        /// <remarks>Not currently supported on Xamarin.Forms</remarks>
-        Refresh,
     }
 }
