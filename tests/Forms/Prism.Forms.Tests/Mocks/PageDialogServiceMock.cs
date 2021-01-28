dd54bff004e26e4820cfebe8112f9785faf3ebdf
Dan Siegel <me@dansiegel.net>
2021-01-28 10:21:32 -0800

    fix PageDialogServiceMock

--- tests/Forms/Prism.Forms.Tests/Mocks/PageDialogServiceMock.cs
+++ tests/Forms/Prism.Forms.Tests/Mocks/PageDialogServiceMock.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using Moq;
+using Prism.AppModel;
 using Prism.Common;
 using Prism.Services;
 
@@ -6,6 +8,14 @@ namespace Prism.Forms.Tests.Mocks
 {
     public class PageDialogServiceMock : PageDialogService
     {
+        private static IKeyboardMapper KeyboardMapper()
+        {
+            var mock = new Mock<IKeyboardMapper>();
+            mock.Setup(x => x.Map(It.IsAny<KeyboardType>()))
+                .Returns(() => Xamarin.Forms.Keyboard.Default);
+            return mock.Object;
+        }
+
         private readonly string pressedButton;
 
         /// <summary>
@@ -13,7 +23,7 @@ namespace Prism.Forms.Tests.Mocks
         /// </summary>
         /// <param name="pressedButton"></param>
         public PageDialogServiceMock(string pressedButton, IApplicationProvider applicationProvider)
-            : base(applicationProvider)
+            : base(applicationProvider, KeyboardMapper())
         {
             this.pressedButton = pressedButton;
         }
