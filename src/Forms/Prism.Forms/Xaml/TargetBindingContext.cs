4bdb4777e1ae4171b32b5da2054ff5a665032a79
Dan Siegel <me@dansiegel.net>
2021-04-20 12:36:40 -0700

    allow override to use old behavior targeting Page ViewModel

---
+++ src/Forms/Prism.Forms/Xaml/TargetBindingContext.cs
@@ -0,0 +1,18 @@
+﻿namespace Prism.Xaml
+{
+    /// <summary>
+    /// Target BindingContext behavior for the <see cref="ParentPageAwareExtension{T}" />
+    /// </summary>
+    public enum TargetBindingContext
+    {
+        /// <summary>
+        /// Use the Target Element's Binding Context
+        /// </summary>
+        Element = 0,
+
+        /// <summary>
+        /// Use the Parent Page's Binding Context (usually the ViewModel)
+        /// </summary>
+        Page = 1
+    }
+}
