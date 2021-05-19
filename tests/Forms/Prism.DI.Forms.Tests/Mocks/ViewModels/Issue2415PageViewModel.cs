06d5762976e1afc1d8b8b4912be628908f52c69e
Dan Siegel <me@dansiegel.net>
2021-05-19 09:47:54 -0700

    add test for #2415

---
+++ tests/Forms/Prism.DI.Forms.Tests/Mocks/ViewModels/Issue2415PageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Regions;
+using Xamarin.Forms;
+
+namespace Prism.DI.Forms.Tests.Mocks.ViewModels
+{
+    public class Issue2415PageViewModel : BindableBase, IInitialize
+    {
+        private readonly IRegionManager _regionManager;
+
+        public Issue2415PageViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize(INavigationParameters parameters)
+{
+            _regionManager.RequestNavigate("ContentRegion", "Issue2415RegionView");
+        }
+    }
+
+    public class Issue2415RegionViewModel
+    {
+
+    }
+}
