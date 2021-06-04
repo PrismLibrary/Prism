8db9a905506d409b5283bdcc2be56b52a5c07c86
Adam Anderson <andersad@gmail.com>
2021-06-04 12:46:29 -0400

    Document view model initialization interfaces

--- src/Forms/Prism.Forms/Navigation/IInitialize.cs
+++ src/Forms/Prism.Forms/Navigation/IInitialize.cs
@@ -1,7 +1,15 @@
 ﻿namespace Prism.Navigation
 {
+    /// <summary>
+    /// Provides synchronous initialization of the ViewModel prior to the View being added to the navigation stack.
+    /// </summary>
+    /// <remarks>For asynchronous initialization see <see cref="IInitializeAsync"/>.</remarks>
     public interface IInitialize
     {
+        /// <summary>
+        /// Called before the implementor has been navigated to.
+        /// </summary>
+        /// <param name="parameters">The navigation parameters.</param>
         void Initialize(INavigationParameters parameters);
     }
 }
--- src/Forms/Prism.Forms/Navigation/IInitializeAsync.cs
+++ src/Forms/Prism.Forms/Navigation/IInitializeAsync.cs
@@ -2,8 +2,16 @@
 
 namespace Prism.Navigation
 {
+    /// <summary>
+    /// Provides asynchronous initialization of the ViewModel prior to the View being added to the navigation stack.
+    /// </summary>
+    /// <remarks>For synchronous initialization see <see cref="IInitialize"/>.</remarks>
     public interface IInitializeAsync
     {
+        /// <summary>
+        /// Called before the implementor has been navigated to.
+        /// </summary>
+        /// <param name="parameters">The navigation parameters.</param>
         Task InitializeAsync(INavigationParameters parameters);
     }
 }
