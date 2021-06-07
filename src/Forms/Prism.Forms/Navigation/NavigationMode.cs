9d90cbd5d1d28cc577da40c143ddd8ce734c9638
Adam Anderson <andersad@gmail.com>
2021-06-07 16:37:25 -0400

    Correct typo in navigation mode documentation

--- src/Forms/Prism.Forms/Navigation/NavigationMode.cs
+++ src/Forms/Prism.Forms/Navigation/NavigationMode.cs
@@ -6,11 +6,11 @@
     public enum NavigationMode
     {
         /// <summary>
-        /// Indicates that a navigation operation occured that resulted in navigating backwards in the navigation stack.
+        /// Indicates that a navigation operation occurred that resulted in navigating backwards in the navigation stack.
         /// </summary>
         Back,
         /// <summary>
-        /// Indicates that a new navigation operation has occured and a new page has been added to the navigation stack.
+        /// Indicates that a new navigation operation has occurred and a new page has been added to the navigation stack.
         /// </summary>
         New,
     }
