a555223afb66dcccc71cb863139fe2bb894cf0a5
Adam Anderson <andersad@gmail.com>
2021-06-07 17:08:31 -0400

    Document the page navigation registry

--- src/Forms/Prism.Forms/Navigation/PageNavigationInfo.cs
+++ src/Forms/Prism.Forms/Navigation/PageNavigationInfo.cs
@@ -3,10 +3,19 @@ using Prism.Navigation;
 
 namespace Prism.Navigation
 {
+    /// <summary>
+    /// Represents page registration information
+    /// </summary>
     public class PageNavigationInfo
     {
+        /// <summary>
+        /// The unique name to registered Page
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// The type of the registered Page
+        /// </summary>
         public Type Type { get; set; }
     }
 }
--- src/Forms/Prism.Forms/Navigation/PageNavigationRegistry.cs
+++ src/Forms/Prism.Forms/Navigation/PageNavigationRegistry.cs
@@ -4,10 +4,18 @@ using System.ComponentModel;
 
 namespace Prism.Navigation
 {
+    /// <summary>
+    /// Maintains page navigation registrations.
+    /// </summary>
     public static class PageNavigationRegistry
     {
         static Dictionary<string, PageNavigationInfo> _pageRegistrationCache = new Dictionary<string, PageNavigationInfo>();
 
+        /// <summary>
+        /// Registers a Page for navigation.
+        /// </summary>
+        /// <param name="name">The unique name to register with the Page.</param>
+        /// <param name="pageType">The type of Page to registe.r</param>
         public static void Register(string name, Type pageType)
         {
             var info = new PageNavigationInfo
@@ -20,6 +28,11 @@ namespace Prism.Navigation
                 _pageRegistrationCache.Add(name, info);
         }
 
+        /// <summary>
+        /// Gets the <see cref="PageNavigationInfo"/> for a given registration name.
+        /// </summary>
+        /// <param name="name">The registration name.</param>
+        /// <returns>The <see cref="PageNavigationInfo"/> instance for the given name if registered, null otherwise.</returns>
         public static PageNavigationInfo GetPageNavigationInfo(string name)
         {
             if (_pageRegistrationCache.ContainsKey(name))
@@ -28,6 +41,11 @@ namespace Prism.Navigation
             return null;
         }
 
+        /// <summary>
+        /// Gets the <see cref="PageNavigationInfo"/> for a given page type.
+        /// </summary>
+        /// <param name="pageType">The registration's type.</param>
+        /// <returns>The first <see cref="PageNavigationInfo"/> instance for the given type if registered, null otherwise.</returns>
         public static PageNavigationInfo GetPageNavigationInfo(Type pageType)
         {
             foreach (var item in _pageRegistrationCache)
@@ -39,11 +57,19 @@ namespace Prism.Navigation
             return null;
         }
 
+        /// <summary>
+        /// Gets the type for a given registration name.
+        /// </summary>
+        /// <param name="name">The registration name.</param>
+        /// <returns>The type of the registration for the given name if registered, null otherwise</returns>
         public static Type GetPageType(string name)
         {
             return GetPageNavigationInfo(name)?.Type;
         }
 
+        /// <summary>
+        /// Clears the page registration cache.
+        /// </summary>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public static void ClearRegistrationCache()
         {
