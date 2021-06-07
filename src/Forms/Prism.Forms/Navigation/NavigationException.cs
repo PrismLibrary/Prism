bd2b0b5b037179f1db688087aa118992e1beb136
Adam Anderson <andersad@gmail.com>
2021-06-07 16:33:09 -0400

    Document navigation exception

--- src/Forms/Prism.Forms/Navigation/NavigationException.cs
+++ src/Forms/Prism.Forms/Navigation/NavigationException.cs
@@ -3,6 +3,9 @@ using Xamarin.Forms;
 
 namespace Prism.Navigation
 {
+    /// <summary>
+    /// Represents errors that occur during navigation.
+    /// </summary>
     public class NavigationException : Exception
     {
         public const string CannotPopApplicationMainPage = "Cannot Pop Application MainPage";
@@ -14,19 +17,36 @@ namespace Prism.Navigation
         public const string ErrorCreatingPage = "An error occurred while resolving the page. This is most likely the result of invalid XAML or other type initialization exception";
         public const string UnknownException = "An unknown error occurred. You may need to specify whether to Use Modal Navigation or not.";
 
+        /// <summary>
+        /// Creates a new NavigationException.
+        /// </summary>
         public NavigationException()
         {
         }
 
+        /// <summary>
+        /// Creates a new NavigationException.
+        /// </summary>
+        /// <param name="message">An error message explaining the probable cause of the navigation error.</param>
+        /// <param name="page">The page that caused the navigation error.</param>
         public NavigationException(string message, Page page) : this(message, page, null)
         {
         }
 
+        /// <summary>
+        /// Creates a new NavigationException.
+        /// </summary>
+        /// <param name="message">An error message explaining the probable cause of the navigation error.</param>
+        /// <param name="page">The page that caused the navigation error.</param>
+        /// <param name="innerException">The exception that caused the navigation error.</param>
         public NavigationException(string message, Page page, Exception innerException) : base(message, innerException)
         {
             Page = page;
         }
 
+        /// <summary>
+        /// Gets the <see cref="Page"/> instance that caused the navigation error.
+        /// </summary>
         public Page Page { get; }
     }
 }
