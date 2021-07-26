d65461ac519254af0809e2fcf7eba76c62ba893f
niimima <niimi@dcinc.co.jp>
2021-07-26 19:01:43 +0900

    Update IModuleGroupsCatalog

--- src/Wpf/Prism.Wpf/Modularity/IModuleGroupsCatalog.cs
+++ src/Wpf/Prism.Wpf/Modularity/IModuleGroupsCatalog.cs
@@ -2,8 +2,16 @@
 
 namespace Prism.Modularity
 {
+    /// <summary>
+    /// Defines a model that can get the collection of <see cref="IModuleCatalogItem"/>.
+    /// </summary>
     public interface IModuleGroupsCatalog
     {
+        /// <summary>
+        /// Gets the items in the <see cref="IModuleCatalog"/>. This property is mainly used to add <see cref="IModuleInfoGroup"/>s or
+        /// <see cref="IModuleInfo"/>s through XAML.
+        /// </summary>
+        /// <value>The items in the catalog.</value>
         Collection<IModuleCatalogItem> Items { get; }
     }
 }
