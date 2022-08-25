0cc89bd1c5221a2923d70092880774fa6c335a71
niimima <niimi@dcinc.co.jp>
2022-08-25 11:56:06 +0900

    Update IDialogResult and DialogResult

--- src/Forms/Prism.Forms/Services/Dialogs/DialogResult.cs
+++ src/Forms/Prism.Forms/Services/Dialogs/DialogResult.cs
@@ -5,9 +5,20 @@ using Prism.Navigation;
 
 namespace Prism.Services.Dialogs
 {
+    /// <summary>
+    /// An <see cref="IDialogResult"/> that contains <see cref="IDialogParameters"/> from the dialog
+    /// and the <see cref="System.Exception"/> of the dialog.
+    /// </summary>
     internal class DialogResult : IDialogResult
     {
+        /// <summary>
+        /// The exception of the dialog.
+        /// </summary>
         public Exception Exception { get; set; }
+
+        /// <summary>
+        /// The parameters from the dialog.
+        /// </summary>
         public IDialogParameters Parameters { get; set; }
     }
 }
--- src/Forms/Prism.Forms/Services/Dialogs/IDialogResult.cs
+++ src/Forms/Prism.Forms/Services/Dialogs/IDialogResult.cs
@@ -2,9 +2,20 @@
 
 namespace Prism.Services.Dialogs
 {
+    /// <summary>
+    /// Contains <see cref="IDialogParameters"/> from the dialog
+    /// and the <see cref="System.Exception"/> of the dialog.
+    /// </summary>
     public interface IDialogResult
     {
+        /// <summary>
+        /// The exception of the dialog.
+        /// </summary>
         Exception Exception { get; }
+
+        /// <summary>
+        /// The parameters from the dialog.
+        /// </summary>
         IDialogParameters Parameters { get; }
     }
 }
