7499437554dabe474d2523a8c30ef336e529dad4
Dan Siegel <me@dansiegel.net>
2023-02-05 21:13:15 -0600

    ci: update workloads

--- build/scripts/xcode-update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-xcodeRoot=/Applications/Xcode_$XcodeOverride.app
-echo "Setting Xcode Override to: $xcodeRoot"
-
-echo '##vso[task.setvariable variable=MD_APPLE_SDK_ROOT;]'$xcodeRoot
-sudo xcode-select --switch $xcodeRoot/Contents/Developer
-
-# Apply temporary workaround for https://github.com/actions/virtual-environments/issues/1932
-rm -f ${HOME}/Library/Preferences/Xamarin/Settings.plist
