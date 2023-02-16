6ce377444dfb176cdefa556f5afc07ac30a78903
Youssef Victor <youssefvictor00@gmail.com>
2023-02-16 21:36:33 +0200

    Delete unused `args` argument

--- e2e/Uno/src/HelloWorld.Skia.Gtk/Program.cs
+++ e2e/Uno/src/HelloWorld.Skia.Gtk/Program.cs
@@ -14,7 +14,7 @@ namespace HelloWorld.Skia.Gtk
                 expArgs.ExitApplication = true;
             };
 
-            var host = new GtkHost(() => new App(), args);
+            var host = new GtkHost(() => new App());
 
             host.Run();
         }
