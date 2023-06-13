73a045392f5f900945d538d5a5ac8bc0d93ad981
Dan Siegel <me@dansiegel.net>
2023-06-13 08:46:53 -0600

    chore: adding xml docs

--- src/Maui/Prism.Maui/Controls/PrismNavigationPage.cs
+++ src/Maui/Prism.Maui/Controls/PrismNavigationPage.cs
@@ -2,13 +2,23 @@
 
 namespace Prism.Controls;
 
+/// <summary>
+/// Provides a wrapper for the NavigationPage to better handle the OnBackButtonPressed event with Prism Navigation
+/// </summary>
 public class PrismNavigationPage : NavigationPage
 {
+    /// <summary>
+    /// Creates a new instance of the <see cref="PrismNavigationPage"/>
+    /// </summary>
     public PrismNavigationPage()
     {
         BackButtonPressed += HandleBackButtonPressed;
     }
 
+    /// <summary>
+    /// Creates a new instance of the <see cref="PrismNavigationPage"/> with a specified <see cref="Page"/> at the Root
+    /// </summary>
+    /// <param name="page"></param>
     public PrismNavigationPage(Page page)
         : base(page)
     {
@@ -17,7 +27,6 @@ public class PrismNavigationPage : NavigationPage
 
     /// <inheritdoc/>
     public event EventHandler BackButtonPressed;
-
 
     /// <inheritdoc/>
     protected override bool OnBackButtonPressed()
