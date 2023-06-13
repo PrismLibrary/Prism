e8bcb1aec32eb1d562b02ddf693f8e1e3af5610c
Dan Siegel <me@dansiegel.net>
2023-06-13 09:11:25 -0600

    chore: ensure a single definition for DialogResult

--- src/Prism.Core/Dialogs/DialogResult.cs
+++ src/Prism.Core/Dialogs/DialogResult.cs
@@ -7,7 +7,6 @@ namespace Prism.Dialogs;
 /// An <see cref="IDialogResult"/> that contains <see cref="IDialogParameters"/> from the dialog
 /// and the <see cref="ButtonResult"/> of the dialog.
 /// </summary>
-#if NET6_0_OR_GREATER
 public record DialogResult : IDialogResult
 {
     /// <summary>
@@ -18,29 +17,10 @@ public record DialogResult : IDialogResult
     /// <summary>
     /// The parameters from the dialog.
     /// </summary>
-    public IDialogParameters Parameters { get; init; }
+    public IDialogParameters Parameters { get; init; } = new DialogParameters();
 
     /// <summary>
     /// The result of the dialog.
     /// </summary>
     public ButtonResult Result { get; init; } = ButtonResult.None;
 }
-#else
-public class DialogResult : IDialogResult
-{
-    /// <summary>
-    /// An <see cref="System.Exception"/> that was thrown by the DialogService
-    /// </summary>
-    public Exception? Exception { get; set; }
-
-    /// <summary>
-    /// The parameters from the dialog.
-    /// </summary>
-    public IDialogParameters Parameters { get; set; }
-
-    /// <summary>
-    /// The result of the dialog.
-    /// </summary>
-    public ButtonResult Result { get; private set; } = ButtonResult.None;
-}
-#endif
