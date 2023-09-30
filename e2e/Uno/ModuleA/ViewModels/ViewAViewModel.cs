17b6b701cb569c6315683ac4df8db9fd2d0f28f5
Dan Siegel <me@dansiegel.net>
2023-09-30 06:46:00 -0600

    chore: adding dialog example to Uno sample

---
+++ e2e/Uno/ModuleA/ViewModels/ViewAViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleA.ViewModels;
+
+internal class ViewAViewModel : ViewModelBase
+{
+    public ViewAViewModel(IRegionManager regionManager)
+        : base(regionManager)
+    {
+    }
+}
