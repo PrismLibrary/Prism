c54ceca28c16bebcd2580768dcba355df44f7f98
Dan Siegel <me@dansiegel.net>
2023-10-07 12:18:49 -0600

    chore: adding DialogSample for Uno App

--- e2e/Uno/ModuleA/ViewModels/ViewAViewModel.cs
+++ e2e/Uno/ModuleA/ViewModels/ViewAViewModel.cs
@@ -1,16 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Input;
 
 namespace ModuleA.ViewModels;
 
 internal class ViewAViewModel : ViewModelBase
 {
-    public ViewAViewModel(IRegionManager regionManager)
+    private readonly IDialogService _dialogService;
+
+    public ViewAViewModel(IRegionManager regionManager, IDialogService dialogService)
         : base(regionManager)
     {
+        _dialogService = dialogService;
+        ShowAlertCommand = new DelegateCommand(ShowDialog);
+    }
+
+    public ICommand ShowAlertCommand { get; }
+
+    private void ShowDialog()
+    {
+        _dialogService.ShowDialog("AlertDialog", new DialogParameters
+        {
+            { "title", "Oh Snap" },
+            { "message", "You can actually create much more amazing dialogs with Prism. Hello from ViewA!" }
+        });
     }
 }
