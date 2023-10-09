fb9198868413d57561e67e17096d87e2805928b9
Dan Siegel <me@dansiegel.net>
2023-10-08 20:05:25 -0600

    chore: fixing breaking changes in app structure

--- e2e/Uno/HelloWorld.Skia.Gtk/Program.cs
+++ e2e/Uno/HelloWorld.Skia.Gtk/Program.cs
@@ -1,21 +1,20 @@
-using System;
 using GLib;
-using Uno.UI.Runtime.Skia;
+using Uno.UI.Runtime.Skia.Gtk;
 
 namespace HelloWorld.Skia.Gtk;
 
 public class Program
 {
-	public static void Main(string[] args)
-	{
-		ExceptionManager.UnhandledException += delegate (UnhandledExceptionArgs expArgs)
-		{
-			Console.WriteLine("GLIB UNHANDLED EXCEPTION" + expArgs.ExceptionObject.ToString());
-			expArgs.ExitApplication = true;
-		};
+    public static void Main(string[] args)
+    {
+        ExceptionManager.UnhandledException += delegate (UnhandledExceptionArgs expArgs)
+        {
+            Console.WriteLine("GLIB UNHANDLED EXCEPTION" + expArgs.ExceptionObject.ToString());
+            expArgs.ExitApplication = true;
+        };
 
-		var host = new GtkHost(() => new AppHead(), args);
+        var host = new GtkHost(() => new AppHead());
 
-		host.Run();
-	}
+        host.Run();
+    }
 }
