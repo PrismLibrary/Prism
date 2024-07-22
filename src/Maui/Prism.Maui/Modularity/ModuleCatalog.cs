3829ce4f470c059512f2a1d5035df47cc7560670
Dan Siegel <me@dansiegel.net>
2024-07-22 10:03:42 -0600

    chore: use explicit ContentProperty
    (cherry picked from commit 1efdbe3aef6cf3d3fd1d372c7c11c0a2acfe2ead)

--- src/Maui/Prism.Maui/Modularity/ModuleCatalog.cs
+++ src/Maui/Prism.Maui/Modularity/ModuleCatalog.cs
@@ -1,6 +1,15 @@
-﻿namespace Prism.Modularity;
+namespace Prism.Modularity;
 
+/// <summary>
+/// The <see cref="ModuleCatalog"/> holds information about the modules that can be used by the
+/// application. Each module is described in a <see cref="ModuleInfo"/> class, that records the
+/// name and type of the module.
+/// </summary>
+#if PRISMUNO
+[Microsoft.UI.Xaml.Markup.ContentProperty(Name = nameof(Items))]
+#else
 [ContentProperty(nameof(Items))]
+#endif
 public class ModuleCatalog : ModuleCatalogBase
 {
 
