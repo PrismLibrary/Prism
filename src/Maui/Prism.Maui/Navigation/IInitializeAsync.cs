dfa80b1eab6a893b12e77789237f3166cef4cfcb
niimima <niimi@dcinc.co.jp>
2024-08-09 16:45:27 +0900

    Add XML Docs comments

--- src/Maui/Prism.Maui/Navigation/IInitializeAsync.cs
+++ src/Maui/Prism.Maui/Navigation/IInitializeAsync.cs
@@ -1,6 +1,13 @@
 ﻿namespace Prism.Navigation;
 
+/// <summary>
+/// Provides a way for ViewModels involved in navigation to asynchronously initialize.
+/// </summary>
 public interface IInitializeAsync
 {
+    /// <summary>
+    /// Initializes this instance.
+    /// </summary>
+    /// <param name="parameters">The navigation parameters.</param>
     Task InitializeAsync(INavigationParameters parameters);
 }
