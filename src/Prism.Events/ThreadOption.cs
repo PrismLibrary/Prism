71189ecba057b5209f4035d12691c3daa4ccecdf
Sachin Kumar <sacpundir@gmail.com>
2026-02-04 08:14:53 +0530

    Fix #1978 — Add XML docs for EventAggregator APIs

--- src/Prism.Events/ThreadOption.cs
+++ src/Prism.Events/ThreadOption.cs
@@ -7,21 +7,37 @@ namespace Prism.Events
     /// <summary>
     /// Specifies on which thread a <see cref="PubSubEvent{TPayload}"/> subscriber will be called.
     /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This enumeration is used when subscribing to Pub/Sub events to control where the subscription callback is executed.
+    /// The choice affects threading behavior and should be considered based on the nature of the work being performed.
+    /// </para>
+    /// </remarks>
     public enum ThreadOption
     {
         /// <summary>
         /// The call is done on the same thread on which the <see cref="PubSubEvent{TPayload}"/> was published.
         /// </summary>
+        /// <remarks>
+        /// This is the default and most performant option. Use this when the callback doesn't need to interact with the UI.
+        /// </remarks>
         PublisherThread,
 
         /// <summary>
         /// The call is done on the UI thread.
         /// </summary>
+        /// <remarks>
+        /// Use this option when the callback needs to update UI elements. On XAML platforms, this typically means the thread
+        /// that created the UI dispatcher. On non-UI platforms, this behaves like PublisherThread.
+        /// </remarks>
         UIThread,
 
         /// <summary>
         /// The call is done asynchronously on a background thread.
         /// </summary>
+        /// <remarks>
+        /// Use this option when the callback performs long-running operations that shouldn't block the publisher thread.
+        /// </remarks>
         BackgroundThread
     }
 }
